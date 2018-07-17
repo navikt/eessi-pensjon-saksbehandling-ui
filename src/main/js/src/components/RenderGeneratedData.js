3e4ff60739f6a35b1ddd4e4eb4a6494a098d53f5
Nuno Cardoso <Nuno.Cardoso@nav.no>
2018-07-17 10:08:03 +0100

    EESSIPEN-12: Generate data render

--- src/main/js/src/components/RenderGeneratedData.js
+++ src/main/js/src/components/RenderGeneratedData.js
@@ -13,7 +13,7 @@ class RenderGeneratedData extends Component {
             let value = json[key];
             if (typeof value === 'string') {
                 if (value !== 'null' && value !== '') {
-                    res.push('<div style={{paddingLeft: "' + padding + '"}}><b>' + key + '</b>: ' + value + '</div>');
+                    res.push(<div style={{paddingLeft:  padding }}><b>{key}</b>: {value}</div>);
                 }
             } else {
                 res.push(this.renderJson(value, level++))
