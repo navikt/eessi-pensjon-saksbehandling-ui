52fbeb16a593fcec27479179bd089b54c9afcc61
Nuno Cardoso <Nuno.Cardoso@nav.no>
2018-09-27 13:09:07 +0100

    Dragging to resize

--- src/components/ui/Drawer/Drawer.js
+++ src/components/ui/Drawer/Drawer.js
@@ -4,9 +4,6 @@ import classNames from 'classnames';
 import { connect } from 'react-redux';
 import { bindActionCreators }  from 'redux';
 
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
-
 import './Drawer.css';
 
 import * as uiActions from '../../../actions/ui';
@@ -25,29 +22,79 @@ const mapDispatchToProps = (dispatch) => {
 
 class Drawer extends Component {
 
+   state = {
+       draggable: false
+   }
+
    toggleDrawer () {
 
        const { actions } = this.props;
+
        actions.toggleDrawer();
    }
 
    changeDrawerWidth(value) {
 
         const { actions } = this.props;
+
         actions.changeDrawerWidth(value);
+   }
+
+   onMouseClick(e) {
+
+       const { draggable } = this.state;
+
+       this.toggleDrawer();
+       if (draggable) {
+           this.setState({
+               draggable: false
+           })
+       }
+   }
+
+   onMouseDown(e) {
+
+       this.setState({
+            draggable: true
+       })
+   }
+
+   onMouseMove(e) {
+
+        const { actions } = this.props;
+        const { draggable } = this.state;
+
+        if (draggable) {
+            let newX = e.clientX >= 10 ? e.clientX : 10;
+            actions.changeDrawerWidth(newX);
+        }
+   }
+
+   onMouseUp(e) {
+
+        const { draggable } = this.state;
+
+        if (draggable) {
+            this.setState({
+                draggable: false
+            })
+        }
    }
 
    render () {
 
        const { children, sidebar, drawerOpen, drawerWidth } = this.props;
+       const { draggable } = this.state;
 
-       return <div id="drawer-container" className={classNames({ toggled : drawerOpen })}>
+       return <div id="drawer-container"
+           className={classNames({ toggled : drawerOpen, draggable : draggable })}
+           onMouseMove={this.onMouseMove.bind(this)}
+           onMouseUp={this.onMouseUp.bind(this)}>
            <div id="drawer" style={{width: drawerWidth}}>
                <div id="drawer-content">
-                   <Slider value={drawerWidth} min={0} max={500} step={1} onChange={this.changeDrawerWidth.bind(this)}/>
                    {sidebar}
                </div>
-               <div id="drawer-button" onClick={this.toggleDrawer.bind(this)}>
+               <div id="drawer-button" onMouseDown={this.onMouseDown.bind(this)} onClick={this.onMouseClick.bind(this)}>
                    {drawerOpen ? '◀' : '▶'}
                </div>
            </div>
