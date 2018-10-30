64be80242f9875084278c7f74260b1b68c84e9a3
erlend-axelsson <erlend.axelsson@gmail.com>
2018-10-30 10:42:14 +0100

    Minor change to s3 save function.

--- src/pages/PInfo/PInfo.js
+++ src/pages/PInfo/PInfo.js
@@ -44,7 +44,7 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 const onSaveButtonClick = (props) => {
-  props.actions.postStorageFile(props.username, storages.PINFO, 'PINFO', JSON.stringify(props.form))
+  props.actions.postStorageFile(props.username, storages.PINFO, 'PINFO', JSON.stringify({form: props.form }))
   props.history.push(routes.PSELV + '?referrer=pinfo')
 }
 
