908ed1d469e4bd8038aa6ad0f15a87eac53cc1e3
erlend-axelsson <erlend.axelsson@gmail.com>
2018-11-20 13:18:10 +0100

    add fileName to storage reducer

--- src/reducers/storage.js
+++ src/reducers/storage.js
@@ -22,7 +22,8 @@ export default function (state = initialState, action = {}) {
 
     case types.STORAGE_GET_SUCCESS: {
       return Object.assign({}, state, {
-        file: action.payload
+        file: action.payload,
+        fileName: action.fileName
       })
     }
 
