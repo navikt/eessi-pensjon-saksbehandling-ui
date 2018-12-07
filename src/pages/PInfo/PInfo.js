6bc7f680f1d02c863b16cd6d47b20d29839a70ff
erlend-axelsson <erlend.axelsson@gmail.com>
2018-12-07 14:34:03 +0100

    removed heatmap.

--- src/pages/PInfo/PInfo.js
+++ src/pages/PInfo/PInfo.js
@@ -54,7 +54,6 @@ class PInfo extends React.Component {
   }
 
   componentDidMount () {
-    window.hj('trigger', 'e207-heatmap')
     window.hj('trigger', 'e207-feedback-no')
     const { location, actions } = this.props
     let referrer = new URLSearchParams(location.search).get('referrer')
