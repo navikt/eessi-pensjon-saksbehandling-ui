1b74b182fdd3198c9a39c68392bf0bc96e6651b3
erlend-axelsson <erlend.axelsson@gmail.com>
2018-12-11 10:16:41 +0100

    lint:fix

--- src/components/ui/Banner/FirstBanner.js
+++ src/components/ui/Banner/FirstBanner.js
@@ -8,11 +8,11 @@ import './Banner.css'
 class Banner extends Component {
   render () {
     const { t, i18Key, veilI18Key } = this.props
-    let key = i18Key? i18Key: 'app-headerTitle'
-    let veilText = veilI18Key? t(veilI18Key): ''
+    let key = i18Key || 'app-headerTitle'
+    let veilText = veilI18Key ? t(veilI18Key) : ''
     return <div className='c-ui-banner'>
       <h1 className='typo-sidetittel'>{t(key)}</h1>
-      <FirstPanel text={veilText}/>
+      <FirstPanel text={veilText} />
     </div>
   }
 }
--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -6,13 +6,12 @@ import LogoHeader from '../../components/ui/Header/LogoHeader'
 import FirstBanner from '../../components/ui/Banner/FirstBanner'
 import * as Nav from '../../components/ui/Nav'
 
-
 class ErrorPage extends Component {
   render () {
-    const {t} = this.props
+    const { t } = this.props
     return <div className='p-firstPage hodefot'>
       <LogoHeader />
-      <FirstBanner/>
+      <FirstBanner />
       <div className='content'>
         <h2>{t('ui:app-errorPageTitle')}</h2>
       </div>
@@ -20,6 +19,4 @@ class ErrorPage extends Component {
   }
 }
 
-
 export default withNamespaces()(ErrorPage)
-
