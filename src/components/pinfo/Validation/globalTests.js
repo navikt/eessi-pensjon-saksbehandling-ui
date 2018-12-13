43ab265ca8f8d187ba1b9ae60dcf11fc92184ee6
Nuno Cardoso <Nuno.Cardoso@nav.no>
2018-12-13 14:41:58 +0000

    person.idAbroad true by default

--- src/components/pinfo/Validation/globalTests.js
+++ src/components/pinfo/Validation/globalTests.js
@@ -1,5 +1,9 @@
 import _ from 'lodash'
 
 export function isPInfoEmpty (pinfo) {
-  return _.isEmpty(pinfo.person) && _.isEmpty(pinfo.bank) && _.isEmpty(pinfo.stayAbroad)
+  let person = _.cloneDeep(pinfo.person)
+  delete person.idAbroad
+  return _.isEmpty(person) &&
+    _.isEmpty(pinfo.bank) &&
+    _.isEmpty(pinfo.stayAbroad)
 }
--- src/reducers/pinfo.js
+++ src/reducers/pinfo.js
@@ -3,7 +3,9 @@ import * as types from '../constants/actionTypes'
 let initialState = {
   isReady: false,
   step: 0,
-  person: {},
+  person: {
+    idAbroad: true
+  },
   bank: {},
   stayAbroad: [],
   work: {},
@@ -13,44 +15,33 @@ let initialState = {
 export default function (state = initialState, action = {}) {
   switch (action.type) {
     case types.PINFO_STEP_SET:
-
       return Object.assign({}, state, {
         step: action.payload
       })
 
     case types.PINFO_PERSON_SET:
-      return {
-        ...state,
-        person: {
-          ...state.person,
-          ...action.payload
-        } }
+      return Object.assign({}, state, {
+        person: action.payload
+      })
 
     case types.PINFO_WORK_SET:
-      return {
-        ...state,
-        work: {
-          ...state.work,
-          ...action.payload
-        } }
+      return Object.assign({}, state, {
+        work: action.payload
+      })
 
     case types.PINFO_BANK_SET:
-      return {
-        ...state,
-        bank: {
-          ...state.bank,
-          ...action.payload
-        } }
+      return Object.assign({}, state, {
+        bank: action.payload
+      })
 
     case types.PINFO_STAY_ABROAD_SET:
 
       let sortedStayAbroad = action.payload.slice()
       sortedStayAbroad.sort((a, b) => { return a.startDate - b.startDate })
 
-      return {
-        ...state,
+      return Object.assign({}, state, {
         stayAbroad: sortedStayAbroad
-      }
+      })
 
     case types.PINFO_SEND_SUCCESS:
 
