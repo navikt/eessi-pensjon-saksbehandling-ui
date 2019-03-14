9b30ccb7b8d63bfdef818f4f29e41178ec5846cc
erlend-axelsson <erlend.axelsson@gmail.com>
2019-03-14 15:55:27 +0100

    Add CountrySelect tests

--- src/components/ui/CountrySelect/CountryOption.test.js
+++ src/components/ui/CountrySelect/CountryOption.test.js
@@ -4,31 +4,33 @@ import { countries } from './CountrySelectData'
 
 describe('CountryOption Rendering', () => {
   it('Renders correctly', () => {
+
     let countryData = [...countries.en, ...countries.nb]
-    let countrySelectProps = { selectProps: { type: 'country' } }
-    let currencySelectProps = { selectProps: { type: 'currency' } }
+    let countrySelectProps = {selectProps: {type: 'country'}}
+    let currencySelectProps = {selectProps: {type: 'currency'}}
 
     let wrapper = shallow(
       <CountryOption
-        value=''
-        label=''
-        selectProps={{ selectProps: {} }}
-        data={{}}
-        innerProps={{}}
-        isSelected={false}
-        isFocused={false}
+        value= ''
+        label= ''
+        selectProps= {{selectProps: {}}}
+        data= {{}}
+        innerProps= {{}}
+        isSelected= {false}
+        isFocused= {false}
       />
     )
     expect(wrapper).toMatchSnapshot()
     expect(wrapper.isEmptyRender()).toEqual(false)
+    
+    for(let data of countryData){
 
-    for (let data of countryData) {
-      wrapper.setProps({ label: data.label, selectProps: countrySelectProps, data: data })
+      wrapper.setProps({label: data.label, selectProps: countrySelectProps, data: data})
 
       expect(wrapper.isEmptyRender()).toEqual(false)
       expect(wrapper.find('span').text()).toEqual(data.label)
 
-      wrapper.setProps({ selectProps: currencySelectProps })
+      wrapper.setProps({selectProps: currencySelectProps})
 
       expect(wrapper.find('span').text()).toEqual((data.currency ? data.currency + ' - ' : '') + data.currencyLabel)
     }
