28dc0daba38f86fb479824ab9508d8c7d41ed8e2
erlend-axelsson <erlend.axelsson@gmail.com>
2019-04-08 15:42:55 +0200

    Add Modal tests

--- src/components/ui/Modal/Modal.test.js
+++ src/components/ui/Modal/Modal.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import Modal from './Modal'
+import ConnectedModal, { Modal } from './Modal'
 
+import { connect } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import * as reducers from '../../../reducers'
 
@@ -9,45 +10,46 @@ const reducer = combineReducers({
   ...reducers
 })
 
-describe('renders correctly', () => {
-  it('Renders without crashing', () => {
-    let mainButtonOnClick = jest.fn()
-    let otherButtonOnClick = jest.fn()
+describe('renders correctly', ()=>{
+    it('Renders without crashing', ()=>{
 
-    let initialState = {
-      ui: {
-        modalOpen: true,
-        modal: {
-          modalTitle: 'MODALTITLE',
-          modalText: 'MODALTEXT',
-          modalButtons: [{
-            main: true,
-            text: 'MODALMAINBUTTONTEXT',
-            onClick: mainButtonOnClick
-          }, {
-            text: 'MODALOTHERBUTTONTEXT',
-            onClick: otherButtonOnClick
-          }]
+        let mainButtonOnClick = jest.fn()
+        let otherButtonOnClick = jest.fn()
+
+        let initialState = {
+            ui: {
+                modalOpen: true,
+                modal: {
+                    modalTitle: 'MODALTITLE',
+                    modalText: 'MODALTEXT',
+                    modalButtons: [{
+                      main: true,
+                      text: 'MODALMAINBUTTONTEXT',
+                      onClick: mainButtonOnClick
+                    }, {
+                      text: 'MODALOTHERBUTTONTEXT',
+                      onClick: otherButtonOnClick
+                    }]
+                }
+            }
         }
-      }
-    }
 
-    let store = createStore(reducer, initialState)
-    let wrapper = shallow(
-      <Modal />,
-      { context: { store } }
-    )
+        let store = createStore(reducer, initialState)
+        let wrapper = shallow(
+            <ConnectedModal />,
+            {context: { store }}
+        )
 
-    expect(wrapper).toMatchSnapshot()
-    expect(wrapper.dive().dive().find('#modalTitle').text()).toEqual('MODALTITLE')
-    expect(wrapper.dive().dive().find('#modalText').text()).toEqual('MODALTEXT')
+        expect(wrapper).toMatchSnapshot()
+        expect(wrapper.dive().dive().find('#modalTitle').text()).toEqual('MODALTITLE')
+        expect(wrapper.dive().dive().find('#modalText').text()).toEqual('MODALTEXT')
 
-    expect(mainButtonOnClick).toHaveBeenCalledTimes(0)
-    wrapper.dive().dive().find('#c-ui-modal-main-button').simulate('click')
-    expect(mainButtonOnClick).toHaveBeenCalled()
+        expect(mainButtonOnClick).toHaveBeenCalledTimes(0)
+        wrapper.dive().dive().find('#c-ui-modal-main-button').simulate('click')
+        expect(mainButtonOnClick).toHaveBeenCalled()
 
-    expect(otherButtonOnClick).toHaveBeenCalledTimes(0)
-    wrapper.dive().dive().find('#c-ui-modal-other-button').simulate('click')
-    expect(otherButtonOnClick).toHaveBeenCalled()
-  })
+        expect(otherButtonOnClick).toHaveBeenCalledTimes(0)
+        wrapper.dive().dive().find('#c-ui-modal-other-button').simulate('click')
+        expect(otherButtonOnClick).toHaveBeenCalled()
+    })
 })
