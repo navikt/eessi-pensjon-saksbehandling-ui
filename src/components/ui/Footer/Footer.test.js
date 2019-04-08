c91f4e2e49f239e384e6eab277dc42fb23a42563
erlend-axelsson <erlend.axelsson@gmail.com>
2019-04-08 14:15:01 +0200

    Add Footer test

--- src/components/ui/Footer/Footer.test.js
+++ src/components/ui/Footer/Footer.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import ConnectedFooter, { Footer } from './Footer'
 
+import { connect } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import * as reducers from '../../../reducers'
 
@@ -9,31 +10,35 @@ const reducer = combineReducers({
   ...reducers
 })
 
-describe('Render File', () => {
-  it('Render without crashing', () => {
-    let wrapper = shallow(<Footer />)
-    expect(wrapper).toMatchSnapshot()
-    expect(wrapper.isEmptyRender()).toEqual(false)
-  })
+describe('Render File', ()=>{
+    it('Render without crashing', ()=>{
+        let wrapper = shallow(<Footer/>)
+        expect(wrapper).toMatchSnapshot()
+        expect(wrapper.isEmptyRender()).toEqual(false)
+    })
+    
+    it('Toggles open/closed with props', ()=>{
+        let wrapper = shallow(<Footer />)
+        
+        expect(wrapper.exists('div.footerButtonClosed') )
+        
+        wrapper.setProps({footerOpen: true})
+        expect(wrapper.exists('div.footerButtonOpen') )
+    })
 
-  it('Toggles open/closed with props', () => {
-    let wrapper = shallow(<Footer />)
+    it('Calls OnClick', () => {
 
-    expect(wrapper.exists('div.footerButtonClosed'))
 
-    wrapper.setProps({ footerOpen: true })
-    expect(wrapper.exists('div.footerButtonOpen'))
-  })
+        let store = createStore(reducer, {})
+    
+        let wrapper = shallow(
+          <ConnectedFooter />,
+          { context: { store } }
+        )
+        expect(store.getState().ui.footerOpen).toEqual(undefined)
+        wrapper.dive().find('div.footerButtonClosed').simulate('click')
+        expect(store.getState().ui.footerOpen).toEqual(true)
+    })
 
-  it('Calls OnClick', () => {
-    let store = createStore(reducer, {})
 
-    let wrapper = shallow(
-      <ConnectedFooter />,
-      { context: { store } }
-    )
-    expect(store.getState().ui.footerOpen).toEqual(undefined)
-    wrapper.dive().find('div.footerButtonClosed').simulate('click')
-    expect(store.getState().ui.footerOpen).toEqual(true)
-  })
 })
