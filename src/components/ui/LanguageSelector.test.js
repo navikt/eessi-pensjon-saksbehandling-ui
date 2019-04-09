9df5a67965b8d3980c4cb7998a794f8af0473095
erlend-axelsson <erlend.axelsson@gmail.com>
2019-04-09 15:29:53 +0200

    Add ColorPicker and LanguageSelector test

--- src/components/ui/LanguageSelector.test.js
+++ src/components/ui/LanguageSelector.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import LanguageSelector from './LanguageSelector'
 
+import { connect } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import * as reducers from '../../reducers'
 
@@ -8,15 +9,15 @@ const reducer = combineReducers({
   ...reducers
 })
 
-describe('Render LanguageSelector', () => {
-  it('Renders without crashing', () => {
-    let store = createStore(reducer, {})
+describe('Render LanguageSelector', ()=>{
+    it('Renders without crashing', ()=>{
+        let store = createStore(reducer, {})
 
-    let wrapper = shallow(
-      <LanguageSelector />,
-      { context: { store } }
-    )
-    expect(wrapper).toMatchSnapshot()
-    expect(wrapper.isEmptyRender()).toEqual(false)
-  })
+        let wrapper = shallow(
+            <LanguageSelector />,
+            {context : {store}}
+        )
+        expect(wrapper).toMatchSnapshot()
+        expect(wrapper.isEmptyRender()).toEqual(false)
+    })
 })
