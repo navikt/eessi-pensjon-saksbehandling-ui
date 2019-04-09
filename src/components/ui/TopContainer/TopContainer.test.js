b3b94409832472ada78aa005c25bcf6982e03cd8
erlend-axelsson <erlend.axelsson@gmail.com>
2019-04-09 10:10:25 +0200

    Add Topcontainer test

--- src/components/ui/TopContainer/TopContainer.test.js
+++ src/components/ui/TopContainer/TopContainer.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import TopContainer from './TopContainer'
 
+import { connect } from 'react-redux'
 import { createStore, combineReducers } from 'redux'
 import * as reducers from '../../../reducers'
 
@@ -11,21 +12,24 @@ const reducer = combineReducers({
   ...reducers
 })
 
-describe('renders', () => {
-  let store = createStore(reducer, {})
 
-  it('renders without crashing', () => {
-    let wrapper = shallow(
-      <TopContainer history={{}} userRole={constants.SAKSBEHANDLER}>
-        <div id='TEST_CHILD' />
-      </TopContainer>,
-      { context: { store } }
-    )
-    expect(wrapper).toMatchSnapshot()
-    expect(wrapper.exists('#TEST_CHILD')).toEqual(true)
-    expect(wrapper.dive().exists({ header: 'TEST_HEADER' })).toEqual(false)
+describe('renders', ()=>{
 
-    wrapper.setProps({ header: 'TEST_HEADER' })
-    expect(wrapper.dive().exists({ header: 'TEST_HEADER' })).toEqual(true)
-  })
+    let store = createStore(reducer, {})
+
+    it('renders without crashing', ()=> {
+        let wrapper = shallow(
+            <TopContainer history={{}} userRole={constants.SAKSBEHANDLER}>
+                <div id='TEST_CHILD' />
+            </TopContainer>,
+            {context: { store }}
+        )
+        expect(wrapper).toMatchSnapshot()
+        expect(wrapper.exists('#TEST_CHILD')).toEqual(true)
+        expect(wrapper.dive().exists({header: 'TEST_HEADER'})).toEqual(false)
+
+        wrapper.setProps({header: 'TEST_HEADER'})
+        expect(wrapper.dive().exists({header: 'TEST_HEADER'})).toEqual(true)
+
+    })
 })
