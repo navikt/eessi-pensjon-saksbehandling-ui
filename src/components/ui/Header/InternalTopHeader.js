f0cdf20cda3b9dca9daf067af37e3817f494dfae
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-04-11 12:00:22 +0100

    Add ID to logo link

--- src/components/ui/Header/InternalTopHeader.js
+++ src/components/ui/Header/InternalTopHeader.js
@@ -52,7 +52,7 @@ export class InternalTopHeader extends Component {
     return <React.Fragment>
       <header className='c-ui-topHeader'>
         <div className='brand'>
-          <a href='#index' onClick={this.onLogoClick.bind(this)}>
+          <a href='#index' id='c-ui-logo-link' onClick={this.onLogoClick.bind(this)}>
             <NavLogoTransparent width='100' height='45' color={highContrast ? 'white' : 'black'} />
           </a>
           <div className='skillelinje' />
