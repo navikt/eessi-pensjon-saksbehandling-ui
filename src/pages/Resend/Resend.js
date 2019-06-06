fa68d09cbc950e1bec6106f53d45ac552cf53560
erlend-axelsson <erlend.axelsson@gmail.com>
2019-06-06 15:41:56 +0200

    Add feedback when using the resubmit endpoint

--- src/pages/Resend/Resend.js
+++ src/pages/Resend/Resend.js
@@ -9,26 +9,55 @@ const CSRF_PROTECTION = cookies.get('NAV_CSRF_PROTECTION')
   ? { 'NAV_CSRF_PROTECTION': cookies.get('NAV_CSRF_PROTECTION') }
   : {}
 
-const submitHandler = (e) => {
-  e.preventDefault()
-  let fileName = document.getElementById('resend-fileName').value
-  fetch(urls.API_SUBMISSION_RESUBMIT_URL, {
-    method: 'POST',
-    crossOrigin: true,
-    json: true,
-    headers: {
-      'Content-Type': 'text/plain; charset=utf-8',
-      ...CSRF_PROTECTION
-    },
-    body: fileName
-  })
-}
-
 export default class Resend extends React.Component {
+  state = {
+    requestStack: []
+  }
+
+  submitHandler = async (e) => {
+    e.preventDefault()
+    let fileName = document.getElementById('resend-fileName').value
+    let response = await fetch(urls.API_SUBMISSION_RESUBMIT_URL, {
+      method: 'POST',
+      crossOrigin: true,
+      json: true,
+      headers: {
+        'Content-Type': 'text/plain; charset=utf-8',
+        ...CSRF_PROTECTION
+      },
+      body: fileName
+    })
+    console.log(response)
+    this.setState({ requestStack: [...this.state.requestStack, { fileName, response }] })
+  }
+
   render () {
     return (
-      <form id='resend-form' onSubmit={submitHandler}>
-        <span>Filnavn <input id='resend-fileName' type='text' /> <input id='resend-submit' type='submit' /></span>
+      <form id='resend-form' onSubmit={this.submitHandler}>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-auto'>
+              <span>Filnavn</span>
+            </div>
+            <div className='col-auto w-75'>
+              <input className='w-100' id='resend-fileName' type='text' />
+            </div>
+            <div className='col-auto'>
+              <input id='resend-submit' type='submit' />
+            </div>
+          </div>
+          {this.state.requestStack.map(request => {
+            let result = request.response.ok ? 'OK' : 'FEILET'
+            let message = request.response.statusText
+            let status = request.response.status
+            let fileName = request.fileName
+            return (
+              <div className='row'>
+                <span>{result}:{message}:{status}:{fileName}</span>
+              </div>
+            )
+          })}
+        </div>
       </form>
     )
   }
