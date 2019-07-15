0760e71b66fd4f198078fe1d21d02bdbee139b5d
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-07-15 09:53:28 +0100

    Remove traces of p4000 and p6000 apps

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -5,12 +5,9 @@ export const INDEX = BASE + '/index'
 
 export const PINFO = BASE + '/pinfo'
 export const PDF = BASE + '/pdf'
-export const P4000 = BASE + '/p4000'
 export const BUC = BASE + '/buc'
 export const NOT_INVITED = BASE + '/notinvited'
 export const NOT_LOGGED = BASE + '/notlogged'
 export const FORBIDDEN = BASE + '/forbidden'
 export const LOGIN = BASE + '/login'
 export const RESEND = '/resend'
-
-export const P4000_ROUTE = P4000 + '/:page?/:mode?'
