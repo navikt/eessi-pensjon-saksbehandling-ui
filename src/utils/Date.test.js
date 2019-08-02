bcbab50e89c08a323ba1f1780a8e1f04c3af8f89
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-08-02 13:30:56 +0100

    Adding unit tests for utils, reducers

---
+++ src/utils/Date.test.js
@@ -0,0 +1,23 @@
+import { renderDate, pinfoDateToDate } from './Date'
+
+describe('utils/Date', () => {
+  const t = jest.fn((translationString) => { return translationString })
+
+  it('renderDate invalid date', () => {
+    expect(renderDate(null, t)).toEqual('ui:unknown')
+  })
+
+  it('renderDate valid date', () => {
+    const date = new Date('2020-12-17T03:24:00')
+    expect(renderDate(date, t)).toEqual('2020.12.17')
+  })
+
+  it('pinfoDateToDate invalid date', () => {
+    expect(pinfoDateToDate(null)).toEqual(null)
+  })
+
+  it('pinfoDateToDate valid date', () => {
+    const date = { month: 11, year: 2020 }
+    expect(pinfoDateToDate(date)).toEqual({ day: 1, month: 10, year: 2020 })
+  })
+})
