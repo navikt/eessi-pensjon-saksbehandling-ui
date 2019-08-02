3b8c43207a78f2a4eeb27e746367b440847c2c54
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-08-02 12:25:39 +0100

    PdfWidget unit test fix

--- src/widgets/PdfWidget.js
+++ src/widgets/PdfWidget.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import PT from 'prop-types'
 import { Link } from 'react-router-dom'
 import { Lenkepanel } from 'components/Nav'
 import * as routes from 'constants/routes'
 
 const PdfWidget = (props) => {
+  const { t, widget } = props
+
   return <div className='w-PdfWidget p-3'>
-    <h4>{props.widget.title}</h4>
+    <h4>{widget.title}</h4>
     <Lenkepanel
       border
       className='link pdfLink'
@@ -15,6 +18,11 @@ const PdfWidget = (props) => {
         {props.t('pdf:app-createPdf')}
       </Lenkepanel>
   </div>
+}
+
+PdfWidget.propTypes = {
+  t: PT.func.isRequired,
+  widget: PT.object.isRequired
 }
 
 export default PdfWidget
