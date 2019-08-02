bdf2a87ed1c8db032478cac653460ef37b8247a8
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-08-02 09:31:39 +0100

    Add unit tests for EkspandertBartWidget, CatMidget

---
+++ src/widgets/CatMidget.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import CatMidget from './CatMidget'
+
+describe('widgets/CatMidget', () => {
+
+  let wrapper
+  const initialMockProps = {
+    onResize: jest.fn()
+  }
+
+  beforeEach(() => {
+    wrapper = mount(<CatMidget {...initialMockProps} />)
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
+  it('Renders', () => {
+    expect(wrapper.isEmptyRender()).toBeFalsy()
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('UseEffect: it tries to resize', () => {
+    expect(initialMockProps.onResize).toHaveBeenCalled()
+  })
+
+  it('Has proper HTML structure', () => {
+    expect(wrapper.exists('.c-d-catMidget')).toBeTruthy()
+    expect(wrapper.find('img')).toBeTruthy()
+  })
+
+  it('Has properties', () => {
+    expect(CatMidget.properties).toHaveProperty('type')
+    expect(CatMidget.properties).toHaveProperty('title')
+    expect(CatMidget.properties).toHaveProperty('description')
+    expect(CatMidget.properties).toHaveProperty('layout')
+    expect(CatMidget.properties).toHaveProperty('options')
+  })
+})
