cc35f6eefc5bef386dc44d23c9373e8bcf3ce3a2
erlend-axelsson <erlend.axelsson@gmail.com>
2019-08-19 13:39:51 +0200

    Moved websocket to IndexPage

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -3,9 +3,30 @@ import PT from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import TopContainer from '../../components/TopContainer/TopContainer'
 import Dashboard from '../../components/Dashboard/Dashboard'
+import { WEBSOCKET_URL } from 'constants/urls'
 import './IndexPage.css'
 
 class IndexPage extends Component {
+
+  //TODO remove/refactor. testing websocket from frontend
+  connectToWebSocket = () => {
+    let conn = new WebSocket(WEBSOCKET_URL, 'v0.eessiBuc')
+    conn.onopen = e => console.log(e.data)
+    conn.onmessage = e => console.log(e.data)
+    conn.onclose = e => console.log(e.data)
+    conn.onerror = e => console.log(e.data)
+    return conn
+  }
+
+  //TODO remove/refactor. testing websocket from frontend
+  componentDidMount() {
+    let websocketConnection
+    if(WEBSOCKET_URL) {
+      websocketConnection = this.connectToWebSocket()
+      websocketConnection.send('{"subscriptions": ["aktoerId"]}')
+    }
+  }
+
   render () {
     const { history, t } = this.props
 
