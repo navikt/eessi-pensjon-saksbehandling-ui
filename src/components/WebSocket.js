446add335d7e0a29809fe65576e6628580e30e35
erlend-axelsson <erlend.axelsson@gmail.com>
2019-08-19 14:19:29 +0200

    Work in progress WebSocket

---
+++ src/components/WebSocket.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import {connect} from 'util/websocket'
+
+const logData = (e) => console.log(e.data)
+
+
+const WebSocket = () => {
+  const [connection, setConnection] = useState(null)
+  const [ready, setReady] = useState(false)
+
+
+  useEffect( () => {
+      let websocketConnection = connect(()=>setReady(true), logData, logData, logData)
+      setConnection(websocketConnection)
+    }, []
+  )
+
+  useEffect( () => {
+    if(ready){
+      connection.send('{"subscriptions": ["foo"]}')
+    }
+
+  }, [ready])
+
+  return <div className='WebSocket'></div>
+
+}
+
+export default WebSocket
--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -3,29 +3,10 @@ import PT from 'prop-types'
 import { withTranslation } from 'react-i18next'
 import TopContainer from '../../components/TopContainer/TopContainer'
 import Dashboard from '../../components/Dashboard/Dashboard'
-import { WEBSOCKET_URL } from 'constants/urls'
+import WebSocket from '../../components/WebSocket'
 import './IndexPage.css'
 
 class IndexPage extends Component {
-
-  //TODO remove/refactor. testing websocket from frontend
-  connectToWebSocket = () => {
-    let conn = new WebSocket(WEBSOCKET_URL, 'v0.eessiBuc')
-    conn.onopen = e => console.log(e.data)
-    conn.onmessage = e => console.log(e.data)
-    conn.onclose = e => console.log(e.data)
-    conn.onerror = e => console.log(e.data)
-    return conn
-  }
-
-  //TODO remove/refactor. testing websocket from frontend
-  componentDidMount() {
-    let websocketConnection
-    if(WEBSOCKET_URL) {
-      websocketConnection = this.connectToWebSocket()
-      websocketConnection.send('{"subscriptions": ["aktoerId"]}')
-    }
-  }
 
   render () {
     const { history, t } = this.props
@@ -35,6 +16,7 @@ class IndexPage extends Component {
       t={t}
       history={history}>
       <Dashboard id='dashboard' />
+      <WebSocket />
     </TopContainer>
   }
 }
