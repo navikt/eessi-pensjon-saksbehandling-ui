481ad072dfc424266479ec0707aadff85383e28e
erlend-axelsson <erlend.axelsson@gmail.com>
2019-08-26 13:46:59 +0200

    Add loading test for new reducer cases

--- src/reducers/loading.test.js
+++ src/reducers/loading.test.js
@@ -29,6 +29,21 @@ describe('reducers/loading', () => {
     return simulate(type, param, true, 'ERROR')
   }
 
+  const simulateNullFailure = (type, param) => {
+    return expect(
+      loadingReducer({
+        ...initialLoadingState,
+        [param]: true
+      }, {
+        type: type
+      })
+    ).toEqual({
+      ...initialLoadingState,
+      [param]: null,
+      status: 'ERROR'
+    })
+  }
+
   it('SOMETHING_ERROR', () => {
     expect(
       loadingReducer({}, {
@@ -98,7 +113,19 @@ describe('reducers/loading', () => {
   })
 
   it('BUC_GET_BUCS_FAILURE', () => {
-    simulateFailure(types.BUC_GET_BUCS_FAILURE, 'gettingBUCs')
+    simulateNullFailure(types.BUC_GET_BUCS_FAILURE, 'gettingBUCs')
+  })
+
+  it('BUC_GET_AVDOD_BUCS_REQUEST', () => {
+    simulateRequest(types.BUC_GET_AVDOD_BUCS_REQUEST, 'gettingAvdodBUCs')
+  })
+
+  it('BUC_GET_AVDOD_BUCS_SUCCESS', () => {
+    simulateSuccess(types.BUC_GET_AVDOD_BUCS_SUCCESS, 'gettingAvdodBUCs')
+  })
+
+  it('BUC_GET_AVDOD_BUCS_FAILURE', () => {
+    simulateNullFailure(types.BUC_GET_AVDOD_BUCS_FAILURE, 'gettingAvdodBUCs')
   })
 
   it('BUC_GET_BUCSINFO_REQUEST', () => {
