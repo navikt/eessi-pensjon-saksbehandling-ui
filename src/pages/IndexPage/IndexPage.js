6e64929ed2b4a2cc02dc6f9f1c29d5e1701a7361
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-10-28 13:28:51 +0100

    Remove Varsel widget from default layout, still allow to add it

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -7,25 +7,23 @@ import { Dashboard } from 'eessi-pensjon-ui'
 import * as extraWidgets from 'widgets'
 import './IndexPage.css'
 
-const defaultLayout = {
-  default: {
+const defaultLayout = [{
+  label: 'default',
+  body: {
     lg: [
       { i: 'w-1-overview', x: 0, y: 0, w: 12, h: 1, minW: 6, maxW: 12, minH: 1, maxH: 999 },
-      { i: 'w-2-buc', x: 0, y: 2, w: 12, h: 6, minW: 6, maxW: 12, minH: 2, maxH: 999 },
-      { i: 'w-3-varsler', x: 0, y: 8, w: 12, h: 2, minW: 6, maxW: 12, minH: 2, maxH: 999 }
+      { i: 'w-2-buc', x: 0, y: 2, w: 12, h: 6, minW: 6, maxW: 12, minH: 2, maxH: 999 }
     ],
     md: [
       { i: 'w-1-overview', x: 0, y: 0, w: 3, h: 1, minW: 2, maxW: 3, minH: 1, maxH: 999 },
-      { i: 'w-2-buc', x: 0, y: 2, w: 3, h: 6, minW: 2, maxW: 3, minH: 2, maxH: 999 },
-      { i: 'w-3-varsler', x: 0, y: 8, w: 3, h: 2, minW: 2, maxW: 3, minH: 2, maxH: 999 }
+      { i: 'w-2-buc', x: 0, y: 2, w: 3, h: 6, minW: 2, maxW: 3, minH: 2, maxH: 999 }
     ],
     sm: [
       { i: 'w-1-overview', x: 0, y: 0, w: 1, h: 1, minW: 1, maxW: 1, minH: 1, maxH: 999 },
-      { i: 'w-2-buc', x: 0, y: 2, w: 1, h: 6, minW: 1, maxW: 1, minH: 2, maxH: 999 },
-      { i: 'w-3-varsler', x: 0, y: 8, w: 1, h: 2, minW: 1, maxW: 1, minH: 2, maxH: 999 }
+      { i: 'w-2-buc', x: 0, y: 2, w: 1, h: 6, minW: 1, maxW: 1, minH: 2, maxH: 999 }
     ]
   }
-}
+}]
 
 const defaultWidgets = [{
   i: 'w-1-overview',
--- src/widgets/Varsler/VarslerWidget.js
+++ src/widgets/Varsler/VarslerWidget.js
@@ -31,8 +31,8 @@ VarslerWidget.properties = {
   title: 'Varsler widget',
   description: 'Varsler widget',
   layout: {
-    lg: { minW: 6, maxW: 12, defaultW: 6, minH: 2, defaultH: 4, maxH: 999 },
-    md: { minW: 3, maxW: 3, defaultW: 1, minH: 2, defaultH: 4, maxH: 999 },
+    lg: { minW: 6, maxW: 12, defaultW: 12, minH: 2, defaultH: 4, maxH: 999 },
+    md: { minW: 3, maxW: 3, defaultW: 3, minH: 2, defaultH: 4, maxH: 999 },
     sm: { minW: 1, maxW: 1, defaultW: 1, minH: 2, defaultH: 4, maxH: 999 }
   },
   options: {
