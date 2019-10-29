c13afdcdf9a60f6bde5b8deb0b2d48a51212cfbf
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-10-29 12:06:28 +0100

    Add Dashboard exception for varsler panel test units

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PT from 'prop-types'
 import ReactTooltip from 'react-tooltip'
+import { connect } from 'store'
 import { withTranslation } from 'react-i18next'
 import TopContainer from 'components/TopContainer/TopContainer'
 import { Dashboard } from 'eessi-pensjon-ui'
@@ -21,6 +22,28 @@ const defaultLayout = [{
     sm: [
       { i: 'w-1-overview', x: 0, y: 0, w: 1, h: 1, minW: 1, maxW: 1, minH: 1, maxH: 999 },
       { i: 'w-2-buc', x: 0, y: 2, w: 1, h: 6, minW: 1, maxW: 1, minH: 2, maxH: 999 }
+    ]
+  }
+}]
+
+const defaultLayoutWithVarsel = [{
+  label: 'default',
+  body: {
+    lg: [
+      { i: 'w-1-overview', x: 0, y: 0, w: 12, h: 1, minW: 6, maxW: 12, minH: 1, maxH: 999 },
+      { i: 'w-2-buc', x: 0, y: 2, w: 12, h: 6, minW: 6, maxW: 12, minH: 2, maxH: 999 },
+      { i: 'w-3-varsler', x: 0, y: 8, w: 12, h: 2, minW: 6, maxW: 12, minH: 2, maxH: 999 }
+    ],
+    md: [
+      { i: 'w-1-overview', x: 0, y: 0, w: 3, h: 1, minW: 2, maxW: 3, minH: 1, maxH: 999 },
+      { i: 'w-2-buc', x: 0, y: 2, w: 3, h: 6, minW: 2, maxW: 3, minH: 2, maxH: 999 },
+      { i: 'w-3-varsler', x: 0, y: 8, w: 3, h: 2, minW: 2, maxW: 3, minH: 2, maxH: 999 }
+
+    ],
+    sm: [
+      { i: 'w-1-overview', x: 0, y: 0, w: 1, h: 1, minW: 1, maxW: 1, minH: 1, maxH: 999 },
+      { i: 'w-2-buc', x: 0, y: 2, w: 1, h: 6, minW: 1, maxW: 1, minH: 2, maxH: 999 },
+      { i: 'w-3-varsler', x: 0, y: 8, w: 1, h: 2, minW: 1, maxW: 1, minH: 2, maxH: 999 }
     ]
   }
 }]
@@ -59,12 +82,14 @@ const defaultConfig = {
 
 const allowedWidgets = ['buc', 'varsler', 'overview']
 
-export const IndexPage = ({ history, t }) => {
+const mapStateToProps = (state) => ({
+  username: state.app.username
+})
+
+export const IndexPage = ({ history, t, username }) => {
   const afterLayoutChange = () => {
     ReactTooltip.rebuild()
   }
-
-
   return (
     <TopContainer
       className='p-indexPage'
@@ -76,7 +101,7 @@ export const IndexPage = ({ history, t }) => {
         id='eessi-pensjon-ui-fss'
         extraWidgets={extraWidgets}
         defaultWidgets={defaultWidgets}
-        defaultLayout={defaultLayout}
+        defaultLayout={username === 'Z990706' ? defaultLayoutWithVarsel : defaultLayout}
         defaultConfig={defaultConfig}
         allowedWidgets={allowedWidgets}
         afterLayoutChange={afterLayoutChange}
@@ -87,7 +112,8 @@ export const IndexPage = ({ history, t }) => {
 
 IndexPage.propTypes = {
   history: PT.object.isRequired,
-  t: PT.func.isRequired
+  t: PT.func.isRequired,
+  username: PT.string
 }
 
-export default withTranslation()(IndexPage)
+export default connect(mapStateToProps, () => {})(withTranslation()(IndexPage))
