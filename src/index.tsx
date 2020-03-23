4df40fc1566cb073a655edcbd02b530acdd29687
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-03-23 11:52:11 +0100

    Trying core-js

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 // IE11
-import 'core-js/es/object/assign'
+import 'core-js'
+/*import 'core-js/es/object/assign'
 import 'core-js/es/object/entries'
 import 'core-js/es/object/keys'
 import 'core-js/es/array/includes'
 import 'core-js/es/array/find'
 import 'core-js/es/array/flat'
 import 'core-js/es/number'
+import 'core-js/es/number/is-finite'
 import 'core-js/es/map'
 import 'core-js/es/set'
 import 'core-js/stable/number'
 import 'core-js/stable/promise'
 import 'core-js/stable/url-search-params'
-
+*/
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { createBrowserHistory } from 'history'
