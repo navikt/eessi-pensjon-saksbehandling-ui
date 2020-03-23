22da4c059ad8da8d077a622cd0f3511c987f9e47
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-03-23 12:14:31 +0100

    trying regenerator-runtime

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,7 @@
 // IE11
-import 'core-js'
-/*import 'core-js/es/object/assign'
-import 'core-js/es/object/entries'
-import 'core-js/es/object/keys'
-import 'core-js/es/array/includes'
-import 'core-js/es/array/find'
-import 'core-js/es/array/flat'
-import 'core-js/es/number'
-import 'core-js/es/number/is-finite'
-import 'core-js/es/map'
-import 'core-js/es/set'
-import 'core-js/stable/number'
-import 'core-js/stable/promise'
-import 'core-js/stable/url-search-params'
-*/
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { createBrowserHistory } from 'history'
