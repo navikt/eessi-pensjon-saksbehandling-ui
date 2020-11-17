24fdaac193b1c37e52396232da8542b812bbf871
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-11-17 12:53:04 +0100

    Fix locale files

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,7 +13,7 @@ i18n
       default: ['nb']
     },
     debug: false,
-    ns: ['ui', 'pinfo', 'buc'],
+    ns: ['ui', 'buc'],
     defaultNS: 'ui',
     backend: {
       loadPath: '/locales/{{lng}}/{{ns}}.json'
