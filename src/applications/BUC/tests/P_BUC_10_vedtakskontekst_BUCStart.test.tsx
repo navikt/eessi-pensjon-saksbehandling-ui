927e2f119266ec171f50dfb4179e6506c7080435
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-12-18 15:24:31 +0100

    Adding unit tests for EP-1046 P_BUC_10 vedtakskontekst

--- src/applications/BUC/tests/P_BUC_10_vedtakskontekst_BUCStart.test.tsx
+++ src/applications/BUC/tests/P_BUC_10_vedtakskontekst_BUCStart.test.tsx
@@ -9,6 +9,7 @@ import mockSubjectAreaList from 'mocks/buc/subjectAreaList'
 import mockTagsList from 'mocks/buc/tagsList'
 import React from 'react'
 import { stageSelector } from 'setupTests'
+import mockPersonAvdods from 'mocks/app/personAvdod'
 
 jest.mock('actions/buc', () => ({
   cleanNewlyCreatedBuc: jest.fn(),
@@ -103,4 +104,137 @@ describe('P_BUC_10 for BUCStart, vedtakkontekst', () => {
     })
   })
 
+  /*
+    EP 1046 - Scenario 4a:
+
+    Gitt at saksbehandler har navigert fra vedtakskontekst
+    OG sakstype er GLENLEV eller BARNEP
+    OG det er kun én avdøde i vedtaket
+    OG saksbehandler velger å opprette P_BUC_10
+    Så vises Avdødes f.nr./d.nr. i Start ny BUC-skjermbildet
+    OG kravdato er preutfylt (kan overskrives av saksbehandler)
+    OG saksbehandler kan opprette P_BUC_10
+    SÅ kan saksbehandler bestille P15000
+    OG Avdødes f.nr./d.nr. vises (kan ikke endres)
+    OG radioknapp "Krav om" preutfylles med "Etterlatteytelser"
+    OG saksbehandler må velge land og mottakerinstitusjon
+    Slik at SED P15000 blir automatisk opprettet i Rina
+    OG SED P15000 blir preutfylt med nødvendig informasjon (hentet fra Pesys, TPS og frontend)
+   */
+  it('Scenario 4 a (frontend): Vedtakskontekst - etterlatteytelser - én avdøde', () => {
+
+    (initialMockProps.onBucChanged as jest.Mock).mockReset();
+    (createBuc as jest.Mock).mockReset()
+
+    stageSelector(defaultSelector, {
+      bucList: ['P_BUC_02', 'P_BUC_10'],
+      personAvdods: mockPersonAvdods(1),
+      pesysContext: constants.VEDTAKSKONTEKST,
+      sakType: SakTypeMap.GJENLEV
+    })
+
+    wrapper = mount(<BUCStart {...initialMockProps} />)
+
+    // initial form inputs
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__buc-select-id\'] input')).toBeTruthy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__avdod-select-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__kravDato-input-id\']')).toBeFalsy()
+
+    // select P_BUC_02
+    let select = wrapper.find('[data-test-id=\'a-buc-c-bucstart__buc-select-id\'] input').hostNodes()
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'Enter' })
+    wrapper.update()
+    expect(initialMockProps.onBucChanged).toHaveBeenCalledWith({
+      label: 'P_BUC_10 - buc:buc-P_BUC_10',
+      value: 'P_BUC_10'
+    })
+
+    // show avdod fnr select
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__avdod-select-id\']')).toBeTruthy()
+    // keep kravDato input hidden
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__kravDato-input-id\']')).toBeTruthy()
+    // click forward button
+    wrapper.find('[data-test-id=\'a-buc-c-bucstart__forward-button-id\']').hostNodes().simulate('click')
+    // no validation errors
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__feiloppsummering-id\']')).toBeFalsy()
+    // submit payload and create BUC
+    expect(createBuc).toHaveBeenCalledWith({
+      avdod: {
+        aktoerId: '2865434801175',
+        etternavn: 'BLYANT',
+        fnr: '12345678902',
+        fornavn: 'FRODIG',
+        fulltNavn: 'BLYANT FRODIG',
+        mellomnavn: null,
+        relasjon: 'REPA',
+      },
+      kravDato: '15-12-2020',
+      buc: 'P_BUC_10',
+      person: undefined
+    })
+  })
+
+  /*
+    EP 1046 - Scenario 4b:
+
+    Gitt at saksbehandler har navigert fra vedtakskontekst
+    OG sakstype er GLENLEV eller BARNEP
+    OG det er to avdøde i vedtaket
+    OG saksbehandler velger å opprette P_BUC_10
+    SÅ må saksbehandler velge hvilken avdøde skal P_BUC_10 opprettes på
+    OG kravdato er preutfylt (kan overskrives av saksbehandler)
+    OG saksbehandler kan opprette P_BUC_10
+    SÅ kan saksbehandler kan bestille P15000
+    OG Avdødes f.nr./d.nr. vises (kan ikke endres)
+    OG radioknapp "Krav om" preutfylles med "Etterlatteytelser"
+    OG saksbehandler må velge land og mottakerinstitusjon
+    Slik at SED P15000 blir automatisk opprettet i Rina
+    OG SED P15000 blir preutfylt med nødvendig informasjon (hentet fra Pesys, TPS og frontend)
+   */
+  it('Scenario 4 a (frontend): Vedtakskontekst - etterlatteytelser - to avdøde', () => {
+
+    (initialMockProps.onBucChanged as jest.Mock).mockReset();
+    (createBuc as jest.Mock).mockReset()
+
+    stageSelector(defaultSelector, {
+      bucList: ['P_BUC_02', 'P_BUC_10'],
+      personAvdods: mockPersonAvdods(2),
+      pesysContext: constants.VEDTAKSKONTEKST,
+      sakType: SakTypeMap.GJENLEV
+    })
+
+    wrapper = mount(<BUCStart {...initialMockProps} />)
+
+    // initial form inputs
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__buc-select-id\'] input')).toBeTruthy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__avdod-select-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__kravDato-input-id\']')).toBeFalsy()
+
+    // select P_BUC_02
+    let select = wrapper.find('[data-test-id=\'a-buc-c-bucstart__buc-select-id\'] input').hostNodes()
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'Enter' })
+    wrapper.update()
+    expect(initialMockProps.onBucChanged).toHaveBeenCalledWith({
+      label: 'P_BUC_10 - buc:buc-P_BUC_10',
+      value: 'P_BUC_10'
+    })
+
+    // show avdod fnr select
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__avdod-select-id\']')).toBeTruthy()
+    // keep kravDato input hidden
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__kravDato-input-id\']')).toBeTruthy()
+    // click forward button
+    wrapper.find('[data-test-id=\'a-buc-c-bucstart__forward-button-id\']').hostNodes().simulate('click')
+    // no validation errors
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-bucstart__feiloppsummering-id\']')).toBeTruthy()
+    expect(wrapper.find('[data-test-id=\'a-buc-c-bucstart__feiloppsummering-id\']').hostNodes().render().text()).toEqual(
+      'buc:form-feiloppsummering' + 'buc:validation-chooseAvdod'
+    )
+    // create buc is not called
+    expect(createBuc).not.toHaveBeenCalled()
+  })
 })
--- src/applications/BUC/tests/P_BUC_10_vedtakskontekst_SEDStart.test.tsx
+++ src/applications/BUC/tests/P_BUC_10_vedtakskontekst_SEDStart.test.tsx
@@ -6,6 +6,7 @@ import mockFeatureToggles from 'mocks/app/featureToggles'
 import personAvdod from 'mocks/app/personAvdod'
 import React from 'react'
 import { stageSelector } from 'setupTests'
+import mockPersonAvdods from 'mocks/app/personAvdod'
 
 jest.mock('actions/buc', () => ({
   createReplySed: jest.fn(),
@@ -64,6 +65,17 @@ describe('P_BUC_10 for SEDStart, vedtakskontekst,', () => {
         name: 'NAV ACCEPTANCE TEST 07'
       }],
       seds: [],
+      addedParams: {
+        kravDato: '15-12-2020',
+        subject: {
+          avdod: {
+            fnr: '12345678902'
+          },
+          gjenlevende: {
+            fnr: '12345678901'
+          }
+        }
+      },
       error: null
     },
     NorwayIsNOTCaseOwner: {
@@ -92,6 +104,17 @@ describe('P_BUC_10 for SEDStart, vedtakskontekst,', () => {
         acronym: 'DEMOSE01'
       }],
       seds: [],
+      addedParams: {
+        kravDato: '15-12-2020',
+        subject: {
+          avdod: {
+            fnr: '12345678902'
+          },
+          gjenlevende: {
+            fnr: '12345678901'
+          }
+        }
+      },
       error: null
     }
   } as Bucs
@@ -160,6 +183,139 @@ describe('P_BUC_10 for SEDStart, vedtakskontekst,', () => {
     expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\']')).toBeTruthy()
     // prefills with Alderspensjon
     expect(wrapper.find('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\'] input[aria-checked=true]').props().value).toEqual('Alderspensjon')
+
+    // does not show avdodOrSoker
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdodorsoker-radiogroup-id\']')).toBeFalsy()
+
+    wrapper.find('[data-test-id=\'a-buc-c-sedstart__forward-button-id\']').hostNodes().simulate('click')
+    // needs institution and country (krav dato is prefilled)
+    expect(wrapper.find('[data-test-id=\'a-buc-c-sedstart__feiloppsummering-id\']').hostNodes().render().text()).toEqual(
+      'buc:form-feiloppsummering' + 'buc:validation-chooseInstitution' + 'buc:validation-chooseCountry'
+    )
+  })
+
+  /*
+    EP 1046: Scenario 4a
+
+    Gitt at saksbehandler har navigert fra vedtakskontekst
+    OG sakstype er GLENLEV eller BARNEP
+    OG det er kun én avdøde i vedtaket
+    OG saksbehandler velger å opprette P_BUC_10
+    Så vises Avdødes f.nr./d.nr. i Start ny BUC-skjermbildet
+    OG kravdato er preutfylt (kan overskrives av saksbehandler)
+    OG saksbehandler kan opprette P_BUC_10
+    SÅ kan saksbehandler bestille P15000
+    OG Avdødes f.nr./d.nr. vises (kan ikke endres)
+    OG radioknapp "Krav om" preutfylles med "Etterlatteytelser"
+    OG saksbehandler må velge land og mottakerinstitusjon
+    Slik at SED P15000 blir automatisk opprettet i Rina
+    OG SED P15000 blir preutfylt med nødvendig informasjon (hentet fra Pesys, TPS og frontend)
+
+   */
+  it('EP-1046 Scenario 4a (frontend): Vedtakskontekst - etterlatteytelser - én avdøde', () => {
+
+    (initialMockProps.onSedChanged as jest.Mock).mockReset()
+    stageSelector(defaultSelector, {
+      pesysContext: VEDTAKSKONTEKST,
+      sakType: SakTypeMap.GJENLEV,
+      sedList: ['P2000', 'P15000'],
+      personAvdods: mockPersonAvdods(1)
+    })
+
+    wrapper = mount(<SEDStart {...initialMockProps} />)
+
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__sed-select-id\'] input')).toBeTruthy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-input-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-div-id\']')).toBeTruthy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravDato-input-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdodorsoker-radiogroup-id\']')).toBeFalsy()
+
+    let select = wrapper.find('[data-test-id=\'a-buc-c-sedstart__sed-select-id\'] input').hostNodes()
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'Enter' })
+    wrapper.update()
+    expect(initialMockProps.onSedChanged).toHaveBeenCalledWith({
+      label: 'P15000 - buc:buc-P15000',
+      value: 'P15000'
+    })
+    // does show avdodFnr as read-only
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-input-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-div-id\']')).toBeTruthy()
+    // does show kravDato
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravDato-input-id\']')).toBeTruthy()
+
+    // does show kravOm
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\']')).toBeTruthy()
+    // prefills with Etterlatteytelser
+    expect(wrapper.find('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\'] input[aria-checked=true]').props().value).toEqual('Etterlatteytelser')
+
+    // does not show avdodOrSoker
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdodorsoker-radiogroup-id\']')).toBeFalsy()
+
+    wrapper.find('[data-test-id=\'a-buc-c-sedstart__forward-button-id\']').hostNodes().simulate('click')
+    // needs institution and country (krav dato is prefilled)
+    expect(wrapper.find('[data-test-id=\'a-buc-c-sedstart__feiloppsummering-id\']').hostNodes().render().text()).toEqual(
+      'buc:form-feiloppsummering' + 'buc:validation-chooseInstitution' + 'buc:validation-chooseCountry'
+    )
+  })
+
+  /*
+    EP 1046: Scenario 4b
+
+    Gitt at saksbehandler har navigert fra vedtakskontekst
+    OG sakstype er GLENLEV eller BARNEP
+    OG det er to avdøde i vedtaket
+    OG saksbehandler velger å opprette P_BUC_10
+    SÅ må saksbehandler velge hvilken avdøde skal P_BUC_10 opprettes på
+    OG kravdato er preutfylt (kan overskrives av saksbehandler)
+    OG saksbehandler kan opprette P_BUC_10
+    SÅ kan saksbehandler kan bestille P15000
+    OG Avdødes f.nr./d.nr. vises (kan ikke endres)
+    OG radioknapp "Krav om" preutfylles med "Etterlatteytelser"
+    OG saksbehandler må velge land og mottakerinstitusjon
+    Slik at SED P15000 blir automatisk opprettet i Rina
+    OG SED P15000 blir preutfylt med nødvendig informasjon (hentet fra Pesys, TPS og frontend)
+
+   */
+  it('EP-1046 Scenario 4b (frontend): Vedtakskontekst - etterlatteytelser - to avdøde', () => {
+
+    (initialMockProps.onSedChanged as jest.Mock).mockReset()
+    stageSelector(defaultSelector, {
+      pesysContext: VEDTAKSKONTEKST,
+      sakType: SakTypeMap.GJENLEV,
+      sedList: ['P2000', 'P15000'],
+      personAvdods: mockPersonAvdods(2)
+    })
+
+    wrapper = mount(<SEDStart {...initialMockProps} />)
+
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__sed-select-id\'] input')).toBeTruthy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-input-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-div-id\']')).toBeTruthy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravDato-input-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdodorsoker-radiogroup-id\']')).toBeFalsy()
+
+    let select = wrapper.find('[data-test-id=\'a-buc-c-sedstart__sed-select-id\'] input').hostNodes()
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'ArrowDown' })
+    select.simulate('keyDown', { key: 'Enter' })
+    wrapper.update()
+    expect(initialMockProps.onSedChanged).toHaveBeenCalledWith({
+      label: 'P15000 - buc:buc-P15000',
+      value: 'P15000'
+    })
+    // does show avdodFnr as read-only
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-input-id\']')).toBeFalsy()
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdod-div-id\']')).toBeTruthy()
+    // does show kravDato
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravDato-input-id\']')).toBeTruthy()
+    // does show kravOm
+    expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\']')).toBeTruthy()
+    // prefills with Etterlatteytelser
+    expect(wrapper.find('[data-test-id=\'a-buc-c-sedstart__kravOm-radiogroup-id\'] input[aria-checked=true]').props().value).toEqual('Etterlatteytelser')
 
     // does not show avdodOrSoker
     expect(wrapper.exists('[data-test-id=\'a-buc-c-sedstart__avdodorsoker-radiogroup-id\']')).toBeFalsy()
