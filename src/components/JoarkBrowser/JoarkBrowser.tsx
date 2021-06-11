8658e1e4bb6cb274081574f96cf0bb7a7d3d68ec
Nuno Cardoso <Nuno.Cardoso@nav.no>
2021-06-11 14:33:42 +0200

    Comment md5

--- src/components/JoarkBrowser/JoarkBrowser.tsx
+++ src/components/JoarkBrowser/JoarkBrowser.tsx
@@ -27,7 +27,7 @@ import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 import Table from 'tabell'
-import md5 from 'md5'
+//import md5 from 'md5'
 
 const ButtonsDiv = styled.div`
   display: flex;
