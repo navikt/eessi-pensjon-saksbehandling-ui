9bd28fb3fda90552d1bc43a1e049eb531b3b98ac
Nuno Cardoso <Nuno.Cardoso@nav.no>
2021-11-09 13:16:56 +0100

    Fixed date diff

--- src/utils/dateDiff.test.ts
+++ src/utils/dateDiff.test.ts
@@ -1,64 +1,96 @@
 import dateDiff from 'utils/dateDiff'
 
 describe('utils/dateDiff', () => {
-  it('works', () => {
-    expect(dateDiff('01.01.1982', '01.01.1982')).toEqual({
+  it('31 day month starting on 01 works like regneark', () => {
+    expect(dateDiff('01.01.1980', '01.01.1980')).toEqual({
       days: 1, months: 0, years: 0
     })
-    expect(dateDiff('01.01.1982', '02.01.1982')).toEqual({
+    expect(dateDiff('01.01.1980', '02.01.1980')).toEqual({
       days: 2, months: 0, years: 0
     })
-    expect(dateDiff('01.01.1982', '29.01.1982')).toEqual({
+    expect(dateDiff('01.01.1980', '29.01.1980')).toEqual({
       days: 29, months: 0, years: 0
     })
-    expect(dateDiff('01.01.1982', '30.01.1982')).toEqual({
+    expect(dateDiff('01.01.1980', '30.01.1980')).toEqual({
+      days: 30, months: 0, years: 0
+    })
+    expect(dateDiff('01.01.1980', '31.01.1980')).toEqual({
       days: 0, months: 1, years: 0
     })
-    expect(dateDiff('01.01.1982', '31.01.1982')).toEqual({
-      days: 0, months: 1, years: 0
-    })
-    expect(dateDiff('01.01.1982', '01.02.1982')).toEqual({
+    expect(dateDiff('01.01.1980', '01.02.1980')).toEqual({
       days: 1, months: 1, years: 0
     })
+  })
 
-    expect(dateDiff('02.01.1982', '01.02.1982')).toEqual({
+  it('31 day month starting on 02 works like regneark', () => {
+    expect(dateDiff('02.01.1980', '31.01.1980')).toEqual({
+      days: 30, months: 0, years: 0
+    })
+    expect(dateDiff('02.01.1980', '01.02.1980')).toEqual({
       days: 0, months: 1, years: 0
     })
-    expect(dateDiff('02.01.1982', '02.02.1982')).toEqual({
+    expect(dateDiff('02.01.1980', '02.02.1980')).toEqual({
       days: 1, months: 1, years: 0
     })
-    expect(dateDiff('02.01.1982', '03.02.1982')).toEqual({
+    expect(dateDiff('02.01.1980', '03.02.1980')).toEqual({
       days: 2, months: 1, years: 0
     })
+  })
 
-    expect(dateDiff('03.01.1982', '01.02.1982')).toEqual({
+  it('31 day month starting on 03 works like regneark', () => {
+    expect(dateDiff('03.01.1980', '31.01.1980')).toEqual({
       days: 29, months: 0, years: 0
     })
-    expect(dateDiff('03.01.1982', '02.02.1982')).toEqual({
+    expect(dateDiff('03.01.1980', '01.02.1980')).toEqual({
+      days: 30, months: 0, years: 0
+    })
+    expect(dateDiff('03.01.1980', '02.02.1980')).toEqual({
       days: 0, months: 1, years: 0
     })
-    expect(dateDiff('03.01.1982', '03.02.1982')).toEqual({
+    expect(dateDiff('03.01.1980', '03.02.1980')).toEqual({
       days: 1, months: 1, years: 0
     })
+  })
 
-    expect(dateDiff('01.02.1982', '01.02.1982')).toEqual({
+  it('February in leap year works', () => {
+    expect(dateDiff('01.02.1980', '01.02.1980')).toEqual({
       days: 1, months: 0, years: 0
     })
-    expect(dateDiff('01.02.1982', '02.02.1982')).toEqual({
+    expect(dateDiff('01.02.1980', '02.02.1980')).toEqual({
       days: 2, months: 0, years: 0
     })
-    expect(dateDiff('01.02.1982', '28.02.1982')).toEqual({
+    expect(dateDiff('01.02.1980', '28.02.1980')).toEqual({
       days: 28, months: 0, years: 0
     })
-    expect(dateDiff('01.02.1982', '29.02.1982')).toEqual({
-      days: 29, months: 0, years: 0
+    expect(dateDiff('01.02.1980', '29.02.1980')).toEqual({
+      days: 0, months: 1, years: 0
     })
+    expect(dateDiff('01.02.1980', '01.03.1980')).toEqual({
+      days: 1, months: 1, years: 0
+    })
+  })
 
+  it('February in non-leap year works', () => {
+    expect(dateDiff('01.02.1981', '01.02.1981')).toEqual({
+      days: 1, months: 0, years: 0
+    })
+    expect(dateDiff('01.02.1981', '02.02.1981')).toEqual({
+      days: 2, months: 0, years: 0
+    })
+    expect(dateDiff('01.02.1981', '28.02.1981')).toEqual({
+      days: 0, months: 1, years: 0
+    })
+    expect(dateDiff('01.02.1981', '01.03.1981')).toEqual({
+      days: 1, months: 1, years: 0
+    })
+  })
+
+  it('1 year works', () => {
     expect(dateDiff('01.01.1982', '29.12.1982')).toEqual({
       days: 29, months: 11, years: 0
     })
     expect(dateDiff('01.01.1982', '30.12.1982')).toEqual({
-      days: 0, months: 0, years: 1
+      days: 30, months: 11, years: 0
     })
     expect(dateDiff('01.01.1982', '31.12.1982')).toEqual({
       days: 0, months: 0, years: 1
@@ -66,9 +98,27 @@ describe('utils/dateDiff', () => {
     expect(dateDiff('01.01.1982', '01.01.1983')).toEqual({
       days: 1, months: 0, years: 1
     })
+  })
 
+  it('1 year works with subtracted days', () => {
+    expect(dateDiff('03.01.1982', '31.12.1982')).toEqual({
+      days: 29, months: 11, years: 0
+    })
+    expect(dateDiff('03.01.1982', '01.01.1983')).toEqual({
+      days: 30, months: 11, years: 0
+    })
+    expect(dateDiff('03.01.1982', '02.01.1983')).toEqual({
+      days: 0, months: 0, years: 1
+    })
+    expect(dateDiff('03.01.1982', '03.01.1983')).toEqual({
+      days: 1, months: 0, years: 1
+    })
+  })
+
+
+  it('2 year works', () => {
     expect(dateDiff('10.01.1982', '08.01.1984')).toEqual({
-      days: 29, months: 11, years: 1
+      days: 30, months: 11, years: 1
     })
     expect(dateDiff('10.01.1982', '09.01.1984')).toEqual({
       days: 0, months: 0, years: 2
@@ -78,6 +128,15 @@ describe('utils/dateDiff', () => {
     })
     expect(dateDiff('10.01.1982', '11.01.1984')).toEqual({
       days: 2, months: 0, years: 2
+    })
+  })
+
+  it('other works', () => {
+    expect(dateDiff('31.07.1997', '19.12.1997')).toEqual({
+      days: 20, months: 4, years: 0
+    })
+    expect(dateDiff('20.07.2015', '07.08.2015')).toEqual({
+      days: 19, months: 0, years: 0
     })
   })
 })
--- src/utils/dateDiff.ts
+++ src/utils/dateDiff.ts
@@ -1,3 +1,4 @@
+import moment, { Moment } from "moment"
 
 export interface DateDiff {
   days?: string | number | null | undefined
@@ -13,31 +14,101 @@ export interface FormattedDateDiff {
   years: string | number
 }
 
+/** This is a transcription of a macro function TRYGDETID found in regneark excel file
+ * Don't think too much -- this is how they calculate dates, it's weird,
+ * just look at the test file to understand the rules
+ */
 const dateDiff = (startdato: string, sluttdato: string): FormattedDateDiff => {
-  const res: FormattedDateDiff = {
-    days: 0, months: 0, years: 0
-  }
 
-  const startDatoPieces: Array<number> = startdato.split('.').map(i => parseInt(i, 10))
-  const sluttDatoPieces: Array<number> = sluttdato.split('.').map(i => parseInt(i, 10))
+  let Fomdato: Moment // C
+  let Tomdato: Moment // D
+  let Tomdatokorr: Moment // 'L
+  let AnslagHeleAar: number // 'M
+  let AnslagNyFomEtterAar: Moment //'N
+  let HeleAar: number //'O
+  let NyFomEtterAar: Moment //'P
+  let AnslagHeleMndr: number // 'Q
+  let AnslagNyFomEtterMndr: Moment //'R
+  let KorrAnslagNyFomEtterMndr: Moment // 'S
+  let HeleMndr: number // 'T
+  let NyFomEtterMndr: Moment //'U
+  let KorrNyFomEtterMndr: Moment //'V
+  let AntallDager: number //'W
 
-  const dayDiff: number = (sluttDatoPieces[0] + 1 - startDatoPieces[0])
-  let leftoverMonth = 0
+  Fomdato = moment(startdato, 'DD.MM.YYYY')
+  Tomdato = moment(sluttdato, 'DD.MM.YYYY')
 
-  if (startDatoPieces[0] === 1) {
-    leftoverMonth = (dayDiff >= 30 ? 1 : 0)
-    res.days = leftoverMonth === 1 ? 0 : dayDiff // do not let above 30
+  // Add a day to end date
+  Tomdatokorr = moment(Tomdato.toDate())
+  Tomdatokorr.add(1, 'days')
+
+  // estimate years based on number of days
+  let numberOfDays = Tomdatokorr!.diff(Fomdato, 'days')
+  AnslagHeleAar = Math.round(numberOfDays / 365)
+
+  // Estimates new start date after whole year was added
+  AnslagNyFomEtterAar = moment(Fomdato.toDate())
+  AnslagNyFomEtterAar.add(AnslagHeleAar, 'years')
+
+  // check if estimeted new startdate is over end date
+  if (AnslagNyFomEtterAar > Tomdatokorr!) {
+    HeleAar = AnslagHeleAar - 1
   } else {
-    // deal with negatives
-    res.days = (dayDiff + 30) % 30 // do not let above 30
+    HeleAar = AnslagHeleAar
   }
 
-  const monthDiff = leftoverMonth + (dayDiff < 0 ? -1 : 0) + (sluttDatoPieces[1] - startDatoPieces[1])
-  const leftoverYear = monthDiff >= 12 ? 1 : 0
+  // sets new startdate after correct number of whole years is calculated
+  NyFomEtterAar = moment(Fomdato.toDate())
+  NyFomEtterAar.add(HeleAar, 'years')
 
-  res.months = leftoverYear ? 0 : (monthDiff >= 0 ? monthDiff : (monthDiff + 12) % 12)
-  res.years = leftoverYear + (monthDiff < 0 ? -1 : 0) + (sluttDatoPieces[2] - startDatoPieces[2])
-  return res
+  // from total days estimate whole months
+  let numberOfDays2 = Tomdatokorr!.diff(NyFomEtterAar, 'days')
+  AnslagHeleMndr = Math.round(numberOfDays2 / 30)
+
+  // set estimation of new stardate after whole month added to new startdate after whole years were added
+
+  AnslagNyFomEtterMndr = moment(NyFomEtterAar.toDate())
+  AnslagNyFomEtterMndr.add(AnslagHeleMndr, 'months')
+
+  KorrAnslagNyFomEtterMndr = moment(AnslagNyFomEtterMndr.toDate())
+
+  // adjust new fom for errors caused by leap days
+  // NOTE: .date() returns the day of the month, as in 13.02.2000 => 13
+  // do NOT confuse with .toDate()
+  if (AnslagNyFomEtterMndr.date() === 28 && Fomdato.date() === 29) {
+    if (AnslagNyFomEtterMndr.month() === (AnslagNyFomEtterMndr.month() + 1)) {
+      KorrAnslagNyFomEtterMndr.add(1, 'days')
+    }
+  }
+
+  // adjust whole month if new startdate is higher than enddate
+  if (KorrAnslagNyFomEtterMndr.isAfter(Tomdatokorr!)) {
+    HeleMndr = AnslagHeleMndr - 1
+  } else {
+    HeleMndr = AnslagHeleMndr
+  }
+
+  NyFomEtterMndr = moment(NyFomEtterAar.toDate())
+  NyFomEtterMndr.add(HeleMndr, 'months')
+
+  KorrNyFomEtterMndr = moment(NyFomEtterMndr.toDate())
+
+  // adjust new fom for errors caused by leap days
+  if (NyFomEtterMndr.date() === 28 && Fomdato.date() === 29) {
+    if (NyFomEtterMndr.month() === (NyFomEtterMndr.month() + 1)) {
+      KorrNyFomEtterMndr.add(1, 'days')
+    }
+  }
+
+  // calculate total days remaining after whole years and months are done
+  AntallDager = Tomdatokorr!.diff(KorrNyFomEtterMndr, 'days')
+
+  return {
+    years: HeleAar,
+    months: HeleMndr,
+    days: AntallDager
+  }
 }
 
 export default dateDiff
+
