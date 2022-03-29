af5895c05bc5bcb60ff1aef4abf3b5ca485c7d62
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-03-29 20:58:15 +0200

    better fallback

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.get('/test', (req, res) => res.send('hello world'));
 app.get('/internal/isAlive|isReady', (req, res) => res.sendStatus(200));
 app.use(frontendProxy)
 app.use(fagmodulProxy)
-app.use('/', express.static(path.join(__dirname, "build")));
+app.use('/static', express.static(path.join(__dirname, "build", "static")));
+app.use('/locales', express.static(path.join(__dirname, "build", "locales")));
+app.use('/favicon', express.static(path.join(__dirname, "build", "favicon")));
+app.use('*', express.static(path.join(__dirname, "build")));
 
 // start express server on port 8080
 app.listen(8080, () => {
