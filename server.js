47a0bcad3b0ceb14d34ae4d070495005dfa95bf6
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-03-29 11:53:25 +0200

    Try 2

--- server.js
+++ server.js
@@ -16,21 +16,21 @@ app.get('/test', (req, res) => {
   res.send('hello world')
 });
 
-app.get(`${basePath}/internal/isAlive|isReady`, (req, res) => res.sendStatus(200));
+app.get('/internal/isAlive|isReady', (req, res) => res.sendStatus(200));
 
-app.get('${basePath}/frontend', function(req,res) {
+app.get('/frontend', function(req,res) {
   //modify the url in any way you want
   var newurl = 'http://eessi-pensjon-frontend-api-fss-q2.dev.intern.nav.no';
   request(newurl).pipe(res);
 });
 
-app.get('${basePath}/fagmodul', function(req,res) {
+app.get('/fagmodul', function(req,res) {
   //modify the url in any way you want
   var newurl = 'http://eessi-pensjon-fagmodul-q2.dev.intern.nav.no';
   request(newurl).pipe(res);
 });
 
-app.use('*', express.static(buildPath, {index: false}));
+app.use('/', express.static(buildPath, {index: false}));
 
 // start express server on port 8080
 app.listen(8080, () => {
