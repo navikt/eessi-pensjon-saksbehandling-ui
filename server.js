0be00a221e9d94b0b6eb65e09492be9dab11aca9
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-03-31 09:47:34 +0200

    Force https to see if it doesn't damage secure cookies

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser')
 
 const enforceAzureADMiddleware = async function(req, res, next) {
 
-  const callbackUrl = req.protocol + '://' + req.host + req.originalUrl
+  const callbackUrl = 'https://' + req.host + req.originalUrl
   const loginUrl = process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL + '/oauth2/login?redirect=' +
     process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL + '/logincallback?redirect=' + encodeURI(callbackUrl)
 
