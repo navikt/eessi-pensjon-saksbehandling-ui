b753f969752239c658e7135d12cc6678fda9bd25
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-03-31 10:13:18 +0200

    Adding auth header to check if already authenticated

--- server.js
+++ server.js
@@ -3,35 +3,25 @@ const path = require('path')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 const cookieParser = require('cookie-parser')
 
+const onProxyReq = function (proxyReq, req, res) {
+  proxyReq.setHeader(
+    "Authorization",
+    "yup-you-are-logged-in"
+  )
+}
+
 const enforceAzureADMiddleware = async function(req, res, next) {
 
   const callbackUrl = 'https://' + req.host + req.originalUrl
   const loginUrl = process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL + '/oauth2/login?redirect=' +
     process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL + '/logincallback?redirect=' + encodeURI(callbackUrl)
 
-  console.log('req.session')
-  console.log(req.session)
-  console.log('req.cookies')
-  console.log(req.cookies)
-  console.log('req.cookies[\'io.nais.wonderwall.session\']')
-  console.log(req.cookies['io.nais.wonderwall.session'])
-
 // Not logged in - log in with wonderwall
-  if (!req.cookies['io.nais.wonderwall.session']) {
+  if (!req.headers.authorization) {
     console.log("no authorization - login to " + loginUrl);
     res.redirect(loginUrl);
   } else {
     next();
-  }
-}
-
-const validateAuthorization = async (authorization) => {
-  try {
-    const token = authorization.split(" ")[1];
-    return !!token
-  } catch (e) {
-    LogError('azure ad error', e);
-    return false;
   }
 }
 
@@ -44,8 +34,8 @@ app.get('/internal/isAlive|isReady|metrics', (req, res) => res.sendStatus(200));
 // Enforce Azure AD authentication
 app.use('/frontend', enforceAzureADMiddleware);
 app.use('/fagmodul', enforceAzureADMiddleware);
-app.use('/frontend', createProxyMiddleware( {target: process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL, changeOrigin: true}))
-app.use('/fagmodul', createProxyMiddleware( {target: process.env.EESSI_PENSJON_FAGMODUL_URL, changeOrigin: true}))
+app.use('/frontend', createProxyMiddleware( {target: process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL, changeOrigin: true, onProxyReq: onProxyReq}))
+app.use('/fagmodul', createProxyMiddleware( {target: process.env.EESSI_PENSJON_FAGMODUL_URL, changeOrigin: true, onProxyReq: onProxyReq}))
 app.use('/static', express.static(path.join(__dirname, "build", "static")));
 app.use('/locales', express.static(path.join(__dirname, "build", "locales")));
 app.use('/favicon', express.static(path.join(__dirname, "build", "favicon")));
