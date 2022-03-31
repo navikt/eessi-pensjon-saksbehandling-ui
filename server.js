704eef632fc4e39a6745686264e12813d49e7516
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-03-31 11:41:37 +0200

    express should not handle oauth requests

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const onProxyReq = function (proxyReq, req, res) {
 const enforceAzureADMiddleware = async function(req, res, next) {
 
   const callbackUrl = 'https://' + req.host + req.originalUrl
-  const loginUrl = '/oauth2/login?redirect=' + encodeURI(callbackUrl)
-
-  console.log(req.headers)
-
+  const loginUrl = '/oauth2/login?redirect=/logincallback?r=' + encodeURI(callbackUrl)
+  
 // Not logged in - log in with wonderwall
   if (!req.headers.authorization) {
     console.log("no authorization - login to " + loginUrl);
@@ -32,15 +30,28 @@ app.disable("x-powered-by");
 app.use(cookieParser());
 
 app.get('/test', (req, res) => res.send('hello world'));
+app.get('/logincallback', (req, res) => {
+  let r = req.query.r
+  console.log('logincallback: ' + r)
+  res.setHeader("Authorization","yup-you-are-good")
+  res.redirect(r);
+});
 app.get('/internal/isAlive|isReady|metrics', (req, res) => res.sendStatus(200));
 // Enforce Azure AD authentication
+app.use('/static', express.static(path.join(__dirname, "build", "static")));
+app.use('/locales', express.static(path.join(__dirname, "build", "locales")));
+app.use('/favicon', express.static(path.join(__dirname, "build", "favicon")));
+
+app.get(["/oauth2/login"], async (req, res) => {
+  res.status(502).send({
+    message: "Wonderwall must handle /oauth2/login",
+  });
+});
+
 app.use('/frontend', enforceAzureADMiddleware);
 app.use('/fagmodul', enforceAzureADMiddleware);
 app.use('/frontend', createProxyMiddleware( {target: process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL, changeOrigin: true, onProxyReq: onProxyReq}))
 app.use('/fagmodul', createProxyMiddleware( {target: process.env.EESSI_PENSJON_FAGMODUL_URL, changeOrigin: true, onProxyReq: onProxyReq}))
-app.use('/static', express.static(path.join(__dirname, "build", "static")));
-app.use('/locales', express.static(path.join(__dirname, "build", "locales")));
-app.use('/favicon', express.static(path.join(__dirname, "build", "favicon")));
 app.use('*', enforceAzureADMiddleware);
 app.use('*', express.static(path.join(__dirname, "build")));
 
