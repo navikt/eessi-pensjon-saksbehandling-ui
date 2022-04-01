0a18c92e6a0d880b42b830832e0c3deb41a3945c
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-04-01 11:38:49 +0200

    Cleaning up code on server

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ const options = {
 };
 
 const azureAdConfig = {
-  tpBackendScope: process.env.TP_BACKEND_SCOPE,
   clientId: process.env.AZURE_APP_CLIENT_ID,
   clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
-  tokenEndpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT,
+  tokenEndpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT
 };
 
 const onBehalfOf = function(scope, assertion) {
@@ -28,7 +27,7 @@ const onBehalfOf = function(scope, assertion) {
   params.append("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
   params.append("client_id", azureAdConfig.clientId);
   params.append("client_secret", azureAdConfig.clientSecret);
-  params.append("scope", azureAdConfig.tpBackendScope);
+  params.append("scope", scope);
   params.append("assertion", assertion);
   params.append("requested_token_use", "on_behalf_of");
 
@@ -58,78 +57,73 @@ app.get(["/oauth2/login"], async (req, res) => {
 });
 
 // require token
-const frontendApiAuth = async function (req, res, next) {
-  logger.info('On frontendApiAuth')
-  if (!req.headers.authorization) {
-    logger.info('redirecting to login')
-    res.redirect("/oauth2/login");
-  } else {
-    try {
-      logger.info('trying onbehalfof with ' + req.headers.authorization)
-      const response = await onBehalfOf(
-        req.headers.authorization.substring("Bearer ".length)
-      );
-
-      const body = await response.json();
-
-      logger.info('got  ' + body.access_token)
-
-      if (response.ok) {
-        res.locals.on_behalf_of_authorization = "Bearer " + body.access_token;
-        next();
-      } else {
-        logger.error(
-          "Error fetching on behalf of token. Status code " + response.status
+const apiAuth = function (scope) {
+  return async function (req, res, next) {
+    logger.info('On apiAuth, with scope ' + scope)
+    if (!req.headers.authorization) {
+      logger.info('redirecting to login')
+      res.redirect("/oauth2/login");
+    } else {
+      try {
+        logger.info('apiAuth: trying onBehalfOf with ' + req.headers.authorization)
+        const response = await onBehalfOf(
+          scope,
+          req.headers.authorization.substring("Bearer ".length)
         );
-        logger.error(body.error_description);
-        logger.error(body);
+
+        const body = await response.json();
+
+        logger.info('apiAuth: got ' + body.access_token)
+
+        if (response.ok) {
+          res.locals.on_behalf_of_authorization = "Bearer " + body.access_token;
+          next();
+        } else {
+          logger.error(
+            "Error fetching on behalf of token. Status code " + response.status
+          );
+          logger.error(body.error_description);
+          logger.error(body);
+          res.redirect("/oauth2/login");
+        }
+      } catch (error) {
+        logger.error(error.message);
         res.redirect("/oauth2/login");
       }
-    } catch (error) {
-      logger.error(error.message);
-      res.redirect("/oauth2/login");
     }
   }
 }
 
-const frontendApiProxy = createProxyMiddleware( {
-  target: process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL,
-  logLevel: 'debug',
-  changeOrigin: true,
-  xfwd: true,
-  pathRewrite:  { '^/frontend' : '/' },
-  onProxyReq: function onProxyReq(proxyReq, req, res) {
-    logger.debug('Request to frontend')
-    logger.debug(req.headers)
-    logger.debug(req.session)
-    logger.info('proxy frontend: adding header auth ' + res.locals.on_behalf_of_authorization)
-    proxyReq.setHeader(
-      "Authorization",
-      res.locals.on_behalf_of_authorization
-    )
-  }
-})
+const apiProxy = function (target, pathRewrite) {
+  logger.info('On apiProxy, with target ' + target)
+  return createProxyMiddleware( {
+    target: target,
+    logLevel: 'debug',
+    changeOrigin: true,
+    xfwd: true,
+    pathRewrite:  pathRewrite,
+    onProxyReq: function onProxyReq(proxyReq, req, res) {
+      logger.debug('onProxyReq')
+      logger.debug(req.headers)
+      logger.debug(req.session)
+      logger.info('proxy frontend: adding header auth ' + res.locals.on_behalf_of_authorization)
+      proxyReq.setHeader(
+        "Authorization",
+        res.locals.on_behalf_of_authorization
+      )
+    }
+  })
+}
 
-app.use('/frontend', frontendApiAuth, frontendApiProxy)
+app.use('/frontend',
+  apiAuth(process.env.EESSI_PENSJON_FRONTEND_API_FSS_TOKEN_SCOPE),
+  apiProxy(process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL,{ '^/frontend' : '/' })
+)
 
-app.use('/fagmodul', createProxyMiddleware( {
-  target: process.env.EESSI_PENSJON_FAGMODUL_URL,
-  logLevel: 'debug',
-  changeOrigin: true,
-  xfwd: true,
-  pathRewrite:  { '^/fagmodul' : '/' },
-  onProxyReq: function onProxyReq(proxyReq, req, res) {
-    logger.debug('Request to fagmodul')
-    logger.debug(req.headers)
-    logger.debug(req.session)
-    logger.info('proxy fagmodul: adding header auth ' + res.locals.on_behalf_of_authorization)
-    proxyReq.setHeader(
-      "Authorization",
-      res.locals.on_behalf_of_authorization
-    )
-  }
-}))
-
+app.use('/fagmodul',
+  apiAuth(process.env.EESSI_PENSJON_FAGMODUL_TOKEN_SCOPE),
+  apiProxy(process.env.EESSI_PENSJON_FAGMODUL_URL,{ '^/fagmodul' : '/' })
+)
 
 app.use('*', express.static(path.join(__dirname, "build")));
 
