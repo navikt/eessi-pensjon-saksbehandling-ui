54599608cdf15d38bf086de30057ebf05b05e635
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-04-28 10:41:40 +0200

    trying prod-gcp

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ const apiProxy = function (target, pathRewrite) {
 
 app.use('/frontend',
   apiAuth(process.env.EESSI_PENSJON_FRONTEND_API_FSS_TOKEN_SCOPE),
-  apiProxy(process.env.EESSI_PENSJON_FRONTEND_API_FSS_URL,{ '^/frontend/' : '/' })
+  apiProxy(process.env.EESSI_PENSJON_FRONTEND_API_URL,{ '^/frontend/' : '/' })
 )
 
 app.use('/fagmodul',
