0337fdefbed4e090af79bfc2368daa26dbf15635
Nuno Cardoso <Nuno.Cardoso@nav.no>
2022-05-11 09:10:46 +0200

    Let login false on prod-gcp

--- server.js
+++ server.js
@@ -83,6 +83,9 @@ const validateAuthorization = async (authorization) => {
 }
 
 const mainPageAuth = async function(req, res, next) {
+  logger.info('mainPageAuth: ' + JSON.stringify(req.query))
+  logger.info('mainPageAuth: ' + req.path)
+  logger.info('mainPageAuth: ' + req.originalUrl)
   const {sakId, aktoerId, vedtakId, kravId, saksNr, sakType} = req.query
   logger.info('mainPageAuth: sakId=' + sakId + ' aktoerId=' + aktoerId + ' vedtakId=' + vedtakId + ' kravId=' + kravId + ' sakType=' + sakType + ' saksNr=' + saksNr )
   const newPath = (aktoerId ?? '-') + '/' + (!!sakId ? sakId : ( !!saksNr ? saksNr : '-')) + '/' + (kravId ?? '-') + '/' + (vedtakId ?? '-') + '/'  + (sakType ?? '-') + '/'
