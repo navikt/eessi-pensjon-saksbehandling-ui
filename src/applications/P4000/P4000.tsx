6d775a2c42fae1722987f0e9c9b7ac11e50d597d
Rittik Dey <dey.rittik@nav.no>
2022-09-08 13:12:27 +0200

    EP-1273: Started P4000 view

--- src/applications/P4000/P4000.tsx
+++ src/applications/P4000/P4000.tsx
@@ -1,13 +1,12 @@
 import { BackFilled } from '@navikt/ds-icons'
 import { Button } from '@navikt/ds-react'
-import { resetSentP5000info } from 'actions/p5000'
-import { P5000Props } from 'applications/P5000/P5000'
 import { BUCMode } from 'declarations/app'
-import { Buc, Sed } from 'declarations/buc'
+import {Buc, Sed, Seds} from 'declarations/buc'
 import { HorizontalSeparatorDiv, VerticalSeparatorDiv } from '@navikt/hoykontrast'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
+import {getSedP4000} from "../../actions/buc";
 
 export interface P4000Props {
   buc: Buc
@@ -15,14 +14,16 @@ export interface P4000Props {
   setMode: (mode: BUCMode, s: string, callback?: () => void, content?: JSX.Element) => void
 }
 
-const P4000: React.FC<P5000Props> = ({
+const P4000: React.FC<P4000Props> = ({
+  buc,
   setMode
 }: P4000Props): JSX.Element => {
   const { t } = useTranslation()
   const dispatch = useDispatch()
 
+  const [_seds, _setSeds] = useState<Seds | undefined>(undefined)
+
   const onBackClick = () => {
-    dispatch(resetSentP5000info())
     setMode('bucedit', 'back')
   }
 
@@ -41,6 +42,15 @@ const P4000: React.FC<P5000Props> = ({
     </div>
   )
 
+  /** check if we need to load SEDs from RINA, when buc changes; send the dispatches */
+  useEffect(() => {
+    // select which P5000 SEDs we want to see
+    const seds = buc.seds?.filter((sed: Sed) => sed.type === 'P4000')
+    _setSeds(seds)
+    seds?.forEach(sed => dispatch(getSedP4000(buc.caseId!, sed)))
+  }, [buc])
+
+  console.log(_seds)
   return (
     <div>
       <VerticalSeparatorDiv size='3' />
