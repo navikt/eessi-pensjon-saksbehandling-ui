97f88137036a71e7de80259bfa9417d336405b38
Rittik Dey <dey.rittik@nav.no>
2022-12-22 16:27:31 +0100

    F!! Add support for newly created BUC. Use common styled components

--- src/applications/BUC/pages/BUCList/BUCListBrukerKontekst.tsx
+++ src/applications/BUC/pages/BUCList/BUCListBrukerKontekst.tsx
@@ -1,9 +1,8 @@
-import {Alert, Heading} from "@navikt/ds-react";
+import {Alert, Button, Heading} from "@navikt/ds-react";
 import {useDispatch, useSelector} from "react-redux";
 import {useTranslation} from "react-i18next";
-import styled from "styled-components/macro";
 import {BUCMode, PesysContext} from "../../../../declarations/app";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {
   fetchBuc,
   fetchBucsInfo,
@@ -12,23 +11,19 @@ import {
   setCurrentBuc
 } from "../../../../actions/buc";
 import {State} from "../../../../declarations/reducers";
-import {BadBucDiv, BucLenkePanel} from "./BUCListBrukerKontekst_V1";
-import {BucInfo, Bucs, BucsInfo, JoarkBuc} from "../../../../declarations/buc";
+import {Buc, BucInfo, Bucs, BucsInfo, JoarkBuc} from "../../../../declarations/buc";
 import BUCHeader from "../../components/BUCHeader/BUCHeader";
 import _ from "lodash";
 import {bucFilter, bucSorter, pbuc02filter} from "../../components/BUCUtils/BUCUtils";
 import {VerticalSeparatorDiv} from "@navikt/hoykontrast";
 import {PersonAvdods} from "../../../../declarations/person";
 import * as storage from "../../../../constants/storage";
+import {buttonLogger} from "../../../../metrics/loggers";
+import classNames from "classnames";
+import {HorizontalLineSeparator} from "../../../../components/StyledComponents";
+import BUCStart from "../../components/BUCStart/BUCStart";
+import {BadBucDiv, BucLenkePanel, BUCListDiv, BUCListHeader, BUCNewDiv, BUCStartDiv} from "../../CommonBucComponents";
 
-export const BUCListDiv = styled.div``
-const BUCListHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  min-height: 40px;
-`
 
 export interface BUCListProps {
   initialBucNew?: boolean
@@ -47,6 +42,7 @@ export interface BUCListBrukerKontekstSelector {
   gettingBuc: boolean
   personAvdods: PersonAvdods | undefined,
   bucs: Bucs | undefined
+  newlyCreatedBuc: Buc | undefined
 }
 
 const mapState = (state: State): BUCListBrukerKontekstSelector => ({
@@ -60,15 +56,16 @@ const mapState = (state: State): BUCListBrukerKontekstSelector => ({
   gettingBucsInfo: state.loading.gettingBucsInfo,
   gettingBuc: state.loading.gettingBuc,
   personAvdods: state.person.personAvdods,
-  bucs: state.buc.bucs
+  bucs: state.buc.bucs,
+  newlyCreatedBuc: state.buc.newlyCreatedBuc,
 })
 const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
-  setMode
+  setMode, initialBucNew = undefined
 }: BUCListProps): JSX.Element => {
 
   const {
     aktoerId, sakId, pesysContext, bucsListJoark, gettingBucsListJoark, bucsInfo, bucsInfoList, gettingBucsInfo,
-    personAvdods, bucs
+    personAvdods, bucs, newlyCreatedBuc
   }: BUCListBrukerKontekstSelector = useSelector<State, BUCListBrukerKontekstSelector>(mapState)
 
   const dispatch = useDispatch()
@@ -77,6 +74,10 @@ const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
   const [_sortedBucs, _setSortedBucs] = useState<Array<JoarkBuc> | undefined>(undefined)
   const [_filteredBucs, _setFilteredBucs] = useState<Array<JoarkBuc> | undefined>(undefined)
   const [_pBuc02filteredBucs, _setPBuc02filteredBucs] = useState<Array<JoarkBuc> | undefined>(undefined)
+  const [_filteredBucsExJoark, _setFilteredBucsExJoark] = useState<Array<Buc> | undefined>(undefined)
+  const [_pBuc02filteredBucsExJoark, _setPBuc02filteredBucsExJoark] = useState<Array<Buc> | undefined>(undefined)
+  const [_sortedBucsExJoark, _setSortedBucsExJoark] = useState<Array<Buc> | undefined>(undefined)
+  const [_newBucPanelOpen, setNewBucPanelOpen] = useState<boolean | undefined>(initialBucNew)
 
 
   useEffect(() => {
@@ -86,6 +87,7 @@ const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
     }
   }, [aktoerId, gettingBucsListJoark, pesysContext, sakId])
 
+  // SORT LIST FROM JOARK
   useEffect(() => {
     if (!_.isEmpty(bucsListJoark)) {
       const filteredBucs: Array<JoarkBuc> = bucsListJoark!.filter(bucFilter)
@@ -97,6 +99,23 @@ const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
     }
   }, [bucsListJoark])
 
+  // SORT BUCS FROM RINA AND NEW BUCS, FILTER OUT BUCS FROM THE JOARK LIST
+  useEffect(() => {
+    if (!_.isEmpty(bucs)) {
+      const filteredBucs: Array<Buc> = Object.keys(bucs!).map(key => bucs![key]).filter(bucFilter)
+      _setFilteredBucsExJoark(filteredBucs)
+      const pBuc02filteredBucs = filteredBucs.filter(pbuc02filter(pesysContext, personAvdods))
+      _setPBuc02filteredBucsExJoark(pBuc02filteredBucs)
+      const sortedBucs = pBuc02filteredBucs.sort(bucSorter)
+      const myBucs = sortedBucs.filter((b) => {
+        return !bucsListJoark?.some((b2) => {
+          return b.caseId === b2.caseId;
+        });
+      })
+      _setSortedBucsExJoark(myBucs)
+    }
+  }, [bucs])
+
   useEffect(() => {
     if (!_.isEmpty(bucsInfoList) && aktoerId && bucsInfo === undefined && !gettingBucsInfo &&
       bucsInfoList!.indexOf(aktoerId + '___' + storage.NAMESPACE_BUC + '___' + storage.FILE_BUCINFO) >= 0) {
@@ -104,9 +123,9 @@ const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
     }
   }, [aktoerId, bucsInfo, bucsInfoList, dispatch, gettingBucsInfo])
 
-  const onBUCEdit = (buc: JoarkBuc): void => {
+  const onBUCEdit = (buc: JoarkBuc | Buc): void => {
     dispatch(setCurrentBuc(buc.caseId!))
-    if (bucs && !bucs[buc.caseId!]) {
+    if (bucs && buc.caseId && !bucs[buc.caseId!]) {
       dispatch(fetchBuc(buc.caseId))
     }
     setMode('bucedit' as BUCMode, 'forward')
@@ -117,14 +136,85 @@ const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
     })
   }
 
+  const onBUCNew = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    buttonLogger(e)
+    setNewBucPanelOpen(true)
+  }
+
+  const BucLenkeCard: React.FC<any> = (props: any): JSX.Element => {
+    const {buc, index} = props;
+    const bucId: string = buc.caseId!
+    const bucInfo: BucInfo = bucsInfo && bucsInfo.bucs && bucsInfo.bucs[bucId] ? bucsInfo.bucs[bucId] : {} as BucInfo
+    return (
+      <BucLenkePanel
+        href='#'
+        border
+        data-testid={'a-buc-p-buclist--buc-' + buc.caseId}
+        key={index}
+        className={classNames({ new: (newlyCreatedBuc && buc.caseId === newlyCreatedBuc.caseId) || false })}
+        style={{ animationDelay: (0.1 * index) + 's' }}
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+          e.preventDefault()
+          e.stopPropagation()
+          onBUCEdit(buc)
+        }}
+      >
+        <BUCHeader
+          buc={buc}
+          bucInfo={bucInfo}
+        />
+      </BucLenkePanel>
+    )
+  }
+
   return (
     <BUCListDiv>
       <BUCListHeader>
         <Heading size='small'>
           {t('buc:form-buclist')}
         </Heading>
+        {!_newBucPanelOpen && (
+          <Button
+            variant='secondary'
+            data-amplitude='buc.list.newbuc'
+            data-testid='a-buc-p-buclist--newbuc-button-id'
+            onClick={onBUCNew}
+          >
+            {t('buc:form-createNewCase')}
+          </Button>
+        )}
       </BUCListHeader>
-      {!_.isNil(_filteredBucs) && !_.isNil(_pBuc02filteredBucs) && _filteredBucs.length !== _pBuc02filteredBucs.length && (
+      <VerticalSeparatorDiv />
+      <BUCStartDiv className={classNames({
+        open: _newBucPanelOpen === true,
+        close: _newBucPanelOpen === false
+      })}
+      >
+        <BUCNewDiv border>
+          <Heading size='medium'>
+            {t('buc:step-startBUCTitle')}
+          </Heading>
+          <HorizontalLineSeparator />
+          <BUCStart
+            aktoerId={aktoerId}
+            onBucCreated={() => {
+              setNewBucPanelOpen(false)
+              setMode('sednew', 'forward')
+              window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+              })
+            }}
+            onBucCancelled={() => setNewBucPanelOpen(false)}
+          />
+        </BUCNewDiv>
+        <VerticalSeparatorDiv />
+      </BUCStartDiv>
+      <VerticalSeparatorDiv />
+
+      {((!_.isNil(_filteredBucs) && !_.isNil(_pBuc02filteredBucs) && _filteredBucs.length !== _pBuc02filteredBucs.length) ||
+        (!_.isNil(_filteredBucsExJoark) && !_.isNil(_pBuc02filteredBucsExJoark) && _filteredBucsExJoark.length !== _pBuc02filteredBucsExJoark.length)) && (
         <>
           <VerticalSeparatorDiv />
           <BadBucDiv>
@@ -135,28 +225,13 @@ const BUCListBrukerKontekst: React.FC<BUCListProps> = ({
           <VerticalSeparatorDiv />
         </>
       )}
+
+      {_sortedBucsExJoark?.map((buc, index: number) => {
+        return (<BucLenkeCard buc={buc} index={index}/>)
+      })}
+
       {_sortedBucs?.map((buc, index: number) => {
-        const bucId: string = buc.caseId!
-        const bucInfo: BucInfo = bucsInfo && bucsInfo.bucs && bucsInfo.bucs[bucId] ? bucsInfo.bucs[bucId] : {} as BucInfo
-        return (
-          <BucLenkePanel
-            href='#'
-            border
-            data-testid={'a-buc-p-buclist--buc-' + buc.caseId}
-            key={index}
-            style={{ animationDelay: (0.1 * index) + 's' }}
-            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-              e.preventDefault()
-              e.stopPropagation()
-              onBUCEdit(buc)
-            }}
-          >
-            <BUCHeader
-              buc={buc}
-              bucInfo={bucInfo}
-            />
-          </BucLenkePanel>
-        )
+        return (<BucLenkeCard buc={buc} index={index}/>)
       })}
     </BUCListDiv>
   )
