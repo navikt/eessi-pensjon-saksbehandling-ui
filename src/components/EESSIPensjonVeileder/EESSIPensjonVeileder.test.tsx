52457877355b8232f0b313480d226ced8b564318
Rittik Dey <dey.rittik@nav.no>
2023-02-10 12:27:32 +0100

    t - Fix test EESSIPensjonVeilder

--- src/components/EESSIPensjonVeileder/EESSIPensjonVeileder.test.tsx
+++ src/components/EESSIPensjonVeileder/EESSIPensjonVeileder.test.tsx
@@ -2,18 +2,22 @@ import { render, screen } from '@testing-library/react'
 import EESSIPensjonVeileder from './EESSIPensjonVeileder'
 
 describe('components/EESSIPensjonVeileder/EESSIPensjonVeileder', () => {
-  let wrapper: any
-
   it('Render: match snapshot', () => {
     const { container } = render(<EESSIPensjonVeileder />)
     expect(container.firstChild).toMatchSnapshot()
   })
 
-  it('Render: chooses veileder correctly', () => {
-    wrapper = render(<EESSIPensjonVeileder mood='smilende' />)
-    expect(screen.getByTestId('c-eessipensjonveileder\']')).toBeTruthy()
-    expect(wrapper.find('img').props().alt).toEqual('nav-smilende-veileder')
-    wrapper.setProps({ mood: 'trist' })
-    expect(wrapper.find('img').props().alt).toEqual('nav-trist-veileder')
+  it('Render: chooses veileder correctly - smilende', () => {
+    render(<EESSIPensjonVeileder mood='smilende' />)
+    const veileder = screen.getByTestId('c-eessipensjonveileder')
+    expect(veileder).toBeInTheDocument()
+    expect(screen.getByRole('img')).toHaveAttribute('alt', 'nav-smilende-veileder')
+  })
+
+  it('Render: chooses veileder correctly - trist', () => {
+    render(<EESSIPensjonVeileder mood='trist' />)
+    const veileder = screen.getByTestId('c-eessipensjonveileder')
+    expect(veileder).toBeInTheDocument()
+    expect(screen.getByRole('img')).toHaveAttribute('alt', 'nav-trist-veileder')
   })
 })
