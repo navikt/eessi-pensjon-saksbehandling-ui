49906387d0eda737404ca7ebef00b6ab7ac7661f
Rittik Dey <dey.rittik@nav.no>
2023-02-10 13:12:36 +0100

    t - Fix test Footer. Add dev-dependency testing-library/userEvent

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,6 +1,7 @@
 import { setStatusParam, unsetStatusParam } from 'actions/app'
 import { toggleFooterOpen } from 'actions/ui'
-import { render } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { stageSelector } from 'setupTests'
 import Footer, { FooterProps } from './Footer'
 
@@ -12,8 +13,6 @@ jest.mock('actions/app', () => ({
   unsetStatusParam: jest.fn()
 }))
 describe('components/Footer', () => {
-  let wrapper: any
-
   const initialMockProps: FooterProps = {
     footerOpen: true,
     params: {}
@@ -28,34 +27,39 @@ describe('components/Footer', () => {
     expect(container.firstChild).toMatchSnapshot()
   })
 
-  it('Handling: Toggles open/closed with props', () => {
-    wrapper = render(<Footer {...initialMockProps} footerOpen={false} />)
-    expect(wrapper.exists('.footerButtonClosed')).toBeTruthy()
-    wrapper.setProps({ footerOpen: true })
-    expect(wrapper.exists('.footerButtonOpen')).toBeTruthy()
+  it('Render: closed', () => {
+    render(<Footer {...initialMockProps} footerOpen={false} />)
+    expect(screen.getByRole('button')).toHaveClass('footerButtonClosed')
   })
+
+  it('Render: open', () => {
+    render(<Footer {...initialMockProps} footerOpen={true} />)
+    expect(screen.getAllByRole('button')[0]).toHaveClass('footerButtonOpen')
+  })
+
+
 
   it('Handling: Toggles open/closed with click', () => {
     (toggleFooterOpen as jest.Mock).mockReset()
-    wrapper = render(<Footer {...initialMockProps} footerOpen={false} />)
-    wrapper.find('.footerButtonClosed').hostNodes().simulate('click')
+    render(<Footer {...initialMockProps} footerOpen={false} />)
+    screen.getByRole('button').click()
     expect(toggleFooterOpen).toHaveBeenCalled()
   })
 
   it('Handling: Adds a param', () => {
     (setStatusParam as jest.Mock).mockReset()
-    wrapper = render(<Footer {...initialMockProps} />)
-    wrapper.find('[data-testid=\'c-footer--select-id').hostNodes().simulate('change', { target: { value: 'aktoerId' } })
-    wrapper.find('[data-testid=\'c-footer--input-id').hostNodes().simulate('change', { target: { value: '123' } })
-    wrapper.find('[data-testid=\'c-footer--add-button-id').hostNodes().simulate('click')
+    render(<Footer {...initialMockProps} />)
+    userEvent.selectOptions(screen.getByTestId('c-footer--select-id'), [screen.getByText('aktoerId')])
+    userEvent.type(screen.getByTestId('c-footer--input-id'), '123')
+    screen.getByTestId('c-footer--add-button-id').click()
     expect(setStatusParam).toHaveBeenCalled()
   })
 
   it('Handling: Remove a param', () => {
     (unsetStatusParam as jest.Mock).mockReset()
-    wrapper = render(<Footer {...initialMockProps} params={{ sakId: '123' }} />)
-    expect(wrapper.find('[data-testid=\'c-footer--param-string\']').hostNodes().render().text()).toEqual('sakId 123')
-    wrapper.find('[data-testid=\'c-footer--remove-button\']').hostNodes().simulate('click')
+    render(<Footer {...initialMockProps} params={{ sakId: '123' }} />)
+    expect(screen.getByTestId('c-footer--param-string')).toHaveTextContent('sakId 123')
+    screen.getByTestId('c-footer--remove-button').click()
     expect(unsetStatusParam).toHaveBeenCalled()
   })
 })
