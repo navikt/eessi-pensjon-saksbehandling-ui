d96f49bc671ea6b24a170d6dc518fd2c18fca12a
Rittik Dey <dey.rittik@nav.no>
2023-03-17 11:06:31 +0100

    b - Add missing mockdata that should not have been commited

--- src/mocks/buc/sed_P5000_small1.ts
+++ src/mocks/buc/sed_P5000_small1.ts
@@ -1,2 +1,318 @@
 export default {
+  sed: 'P5000',
+  sedGVer: '4',
+  sedVer: '2',
+  nav: {
+    eessisak: [{
+      institusjonsid: 'NO:889640782',
+      institusjonsnavn: 'The Norwegian Labour and Welfare Administration',
+      saksnummer: '25183754',
+      land: 'NO'
+    }],
+    bruker: {
+      mor: null,
+      far: null,
+      person: {
+        pin: [{
+          institusjonsnavn: 'The Norwegian Labour and Welfare Administration',
+          institusjonsid: 'NO:889640782',
+          sektor: null,
+          identifikator: '12345678901',
+          land: 'NO',
+          institusjon: null
+        }, {
+          institusjonsnavn: null,
+          institusjonsid: null,
+          sektor: null,
+          identifikator: '1000000000001',
+          land: 'DE',
+          institusjon: null
+        }],
+        pinland: null,
+        statsborgerskap: [{
+          land: 'DE'
+        }],
+        etternavn: 'VON RICHTOFEN',
+        fornavn: 'MANFRED',
+        kjoenn: 'M',
+        foedested: {
+          by: 'Unknown',
+          land: 'DE',
+          region: null
+        },
+        foedselsdato: '1948-01-01',
+        sivilstand: null,
+        relasjontilavdod: null,
+        rolle: null
+      },
+      adresse: {
+        gate: 'GATE 34',
+        bygning: '2000 BRUSSELS',
+        by: 'BELGIA',
+        postnummer: null,
+        region: null,
+        land: 'BE',
+        kontaktpersonadresse: null,
+        datoforadresseendring: null,
+        postadresse: null,
+        startdato: null
+      },
+      arbeidsforhold: null,
+      bank: null
+    },
+    brukere: null,
+    ektefelle: null,
+    barn: null,
+    verge: null,
+    krav: null,
+    sak: null,
+    annenperson: null
+  },
+  pensjon: {
+    trygdetid: [{
+      relevans: null,
+      ordning: null,
+      land: null,
+      sum: {
+        kvartal: null,
+        aar: '2.2222',
+        uker: null,
+        dager: {
+          nr: '00',
+          type: '7'
+        },
+        maaneder: '00'
+      },
+      yrke: null,
+      gyldigperiode: null,
+      type: '10',
+      beregning: '010',
+      informasjonskalkulering: null,
+      periode: null,
+      enkeltkrav: null
+    }],
+    medlemskapAnnen: null,
+    medlemskapTotal: [
+      {
+        relevans: null,
+        ordning: null,
+        land: null,
+        sum: {
+          kvartal: null,
+          aar: '1.2345',
+          uker: null,
+          dager: {
+            nr: '00',
+            type: '7'
+          },
+          maaneder: '00'
+        },
+        yrke: null,
+        gyldigperiode: null,
+        type: '10',
+        beregning: '010',
+        informasjonskalkulering: null,
+        periode: null,
+        enkeltkrav: null
+      },
+      {
+        relevans: null,
+        ordning: null,
+        land: null,
+        sum: {
+          kvartal: null,
+          aar: '1.2345',
+          uker: null,
+          dager: {
+            nr: '00',
+            type: '7'
+          },
+          maaneder: '00'
+        },
+        yrke: null,
+        gyldigperiode: null,
+        type: '11',
+        beregning: '011',
+        informasjonskalkulering: null,
+        periode: null,
+        enkeltkrav: null
+      },
+      {
+        relevans: null,
+        ordning: null,
+        land: null,
+        sum: {
+          kvartal: null,
+          aar: '1.2345',
+          uker: null,
+          dager: {
+            nr: '00',
+            type: '7'
+          },
+          maaneder: '00'
+        },
+        yrke: null,
+        gyldigperiode: null,
+        type: '12',
+        beregning: '100',
+        informasjonskalkulering: null,
+        periode: null,
+        enkeltkrav: null
+      },
+      {
+        relevans: null,
+        ordning: null,
+        land: null,
+        sum: {
+          kvartal: null,
+          aar: '1.2345',
+          uker: null,
+          dager: {
+            nr: '00',
+            type: '7'
+          },
+          maaneder: '00'
+        },
+        yrke: null,
+        gyldigperiode: null,
+        type: '13',
+        beregning: '101',
+        informasjonskalkulering: null,
+        periode: null,
+        enkeltkrav: null
+      }],
+    medlemskap: null,
+    medlemskapboarbeid: {
+      enkeltkrav: {
+        datoFrist: null,
+        krav: '20'
+      },
+      medlemskap: [
+        {
+          relevans: null,
+          ordning: '00',
+          land: null,
+          sum: {
+            kvartal: null,
+            aar: '1',
+            uker: null,
+            dager: {
+              nr: '00',
+              type: '7'
+            },
+            maaneder: '00'
+          },
+          yrke: '0000',
+          gyldigperiode: null,
+          type: '10',
+          beregning: '000',
+          informasjonskalkulering: null,
+          periode: {
+            fom: '1916-01-01',
+            tom: '1916-12-31'
+          },
+          enkeltkrav: null
+        },
+        {
+          relevans: null,
+          ordning: '00',
+          land: null,
+          sum: {
+            kvartal: null,
+            aar: '1',
+            uker: null,
+            dager: {
+              nr: '00',
+              type: '7'
+            },
+            maaneder: '00'
+          },
+          yrke: '0001',
+          gyldigperiode: null,
+          type: '10',
+          beregning: '001',
+          informasjonskalkulering: null,
+          periode: {
+            fom: '1915-01-01',
+            tom: '1915-12-31'
+          },
+          enkeltkrav: null
+        },
+        {
+          relevans: null,
+          ordning: '00',
+          land: null,
+          sum: {
+            kvartal: null,
+            aar: '1',
+            uker: null,
+            dager: {
+              nr: '00',
+              type: '7'
+            },
+            maaneder: '00'
+          },
+          yrke: '0002',
+          gyldigperiode: null,
+          type: '11',
+          beregning: '010',
+          informasjonskalkulering: null,
+          periode: {
+            fom: '1913-01-01',
+            tom: '1913-12-31'
+          },
+          enkeltkrav: null
+        },
+        {
+          relevans: null,
+          ordning: '00',
+          land: null,
+          sum: {
+            kvartal: null,
+            aar: '0',
+            uker: null,
+            dager: {
+              nr: '00',
+              type: '7'
+            },
+            maaneder: '06'
+          },
+          yrke: '0003',
+          gyldigperiode: null,
+          type: '11',
+          beregning: '011',
+          informasjonskalkulering: null,
+          periode: {
+            fom: '1914-01-01',
+            tom: '1914-12-31'
+          },
+          enkeltkrav: null
+        },
+        {
+          relevans: null,
+          ordning: '00',
+          land: null,
+          sum: {
+            kvartal: null,
+            aar: '0',
+            uker: null,
+            dager: {
+              nr: '00',
+              type: '7'
+            },
+            maaneder: '05'
+          },
+          yrke: null,
+          gyldigperiode: null,
+          type: '13',
+          beregning: '100',
+          informasjonskalkulering: null,
+          periode: {
+            fom: '1910-01-01',
+            tom: '1910-12-31'
+          },
+          enkeltkrav: null
+        }]
+    }
+  }
 }
