362b7fb3043be00097dd2033d254c51201d00bb7
Rittik Dey <dey.rittik@nav.no>
2023-05-05 10:41:43 +0200

    f - add mockdata for P2000

---
+++ src/mocks/buc/sed_P2000.ts
@@ -0,0 +1,55 @@
+export default {
+  "sed": "P2000",
+  "nav": {
+    "eessisak": null,
+    "bruker": {
+      "mor": null,
+      "far": null,
+      "person": {
+        "pin": [
+          {
+            "institusjonsnavn": null,
+            "institusjonsid": null,
+            "sektor": null,
+            "identifikator": "11128221249 ",
+            "land": "NO",
+            "institusjon": null
+          },
+          {
+            "institusjonsnavn": null,
+            "institusjonsid": null,
+            "sektor": null,
+            "identifikator": "65 111289 H 006",
+            "land": "DE",
+            "institusjon": null
+          }
+        ],
+        "pinland": null,
+        "statsborgerskap": null,
+        "etternavn": "TRANFLASKE ",
+        "etternavnvedfoedsel": null,
+        "fornavn": "GØYAL",
+        "fornavnvedfoedsel": null,
+        "tidligerefornavn": null,
+        "tidligereetternavn": null,
+        "kjoenn": "K",
+        "foedested": null,
+        "foedselsdato": "1982-12-11",
+        "sivilstand": null,
+        "relasjontilavdod": null,
+        "rolle": null
+      },
+      "adresse": null,
+      "arbeidsforhold": null,
+      "bank": null
+    },
+    "ektefelle": null,
+    "barn": null,
+    "verge": null,
+    "krav": null,
+    "annenperson": null
+  },
+  "pensjon": null,
+  "sedGVer": "4",
+  "sedVer": "2"
+}
