81ec22fd9c5ac0e3bce803bfd5debd5e6404b91f
Rittik Dey <dey.rittik@nav.no>
2023-10-20 09:56:02 +0200

    Fgit add . Remove joark/rina buclist split

--- src/applications/BUC/tests/P_BUC_02_BUCList.test.tsx
+++ src/applications/BUC/tests/P_BUC_02_BUCList.test.tsx
@@ -29,6 +29,7 @@ const mockPersonAvdods = personAvdod(1)
 
 const defaultSelector: BUCListSelector = {
   aktoerId: '123',
+  sakType: undefined,
   bucsList: [{
     euxCaseId: 'NorwayIsCaseOwner',
     buctype: 'P_BUC_02',
