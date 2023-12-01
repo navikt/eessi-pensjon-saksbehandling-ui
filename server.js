7131228a05ee26409aa18978f389dcb4a5777e73
Rittik Dey <dey.rittik@nav.no>
2023-12-01 11:00:45 +0100

    B!! Redirect to originalUrl aftter login

--- server.js
+++ server.js
@@ -79,14 +79,7 @@ const validateAuthorization = async (authorization) => {
 }
 
 const mainPageAuth = async function(req, res, next) {
-  const {sakId, aktoerId, vedtakId, kravId, saksNr, sakType} = req.query
-  const newPath =
-    (aktoerId !== undefined && aktoerId !== '' ? aktoerId : '-') + '/' +
-    (sakId !== undefined && sakId !== '' ? sakId : ( saksNr !== undefined &&  saksNr !== '' ? saksNr : '-')) + '/' +
-    (kravId !== undefined && kravId !== '' ? kravId : '-') + '/' +
-    (vedtakId !== undefined && vedtakId !== '' ? vedtakId :  '-') + '/' +
-    (sakType !== undefined && sakType !== '' ? sakType :  '-') + '/'
-  const loginPath = '/oauth2/login?redirect=/callback/' + newPath
+  const loginPath = '/oauth2/login?redirect='+req.originalUrl
   logger.debug('mainPageAuth: loginPath = ' + loginPath)
   const {authorization} = req.headers
 
