1dad08df3be24ddbe87bf2c182640c1a63a89b31
K153294 <martin.knutsen@nav.no>
2024-02-05 14:07:02 +0100

    B!! Readded old dateDecimal function while implementing new one.

--- src/utils/dateDecimal.ts
+++ src/utils/dateDecimal.ts
@@ -29,12 +29,62 @@ export const sumValues = (...values: any[]): number => {
 }
 
 export const sumDates = (date1: DateDiff, date2: DateDiff, outputAsString = false): FormattedDateDiff => {
-  const res1 = dateDecimal(date1); const res2 = dateDecimal(date2)
-  return dateDecimal({
+  const res1 = dateDecimalOld(date1); const res2 = dateDecimalOld(date2)
+  return dateDecimalOld({
     days: (res1.days as number) + (res2.days as number),
     months: (res1.months as number) + (res2.months as number),
     years: (res1.years as number) + (res2.years as number)
   }, outputAsString)
+}
+
+export const dateDecimalOld = (date: DateDiff, outputAsString = false): FormattedDateDiff => {
+  const res: FormattedDateDiff = {
+    days: 0, months: 0, years: 0
+  }
+  // decimals: worry only to months and years
+  res.days = _.isNil(date.days) ? 0 : _.isNumber(date.days) ? date.days : date.days === '' ? 0 : parseFloat(date.days)
+  res.months = (_.isNil(date.months) ? 0 : _.isNumber(date.months) ? date.months : date.months === '' ? 0 : parseFloat(date.months))
+  res.years = (_.isNil(date.years) ? 0 : _.isNumber(date.years) ? date.years : date.years === '' ? 0 : parseFloat(date.years))
+
+  let needToRecalculate = false
+  if (Object.prototype.hasOwnProperty.call(date, 'trimesters')) {
+    const moreMonths = _.isNil(date.trimesters) ? 0 : _.isNumber(date.trimesters) ? date.trimesters * 3.0 : parseFloat(date.trimesters!) * 3.0
+    res.months += moreMonths
+    if (moreMonths !== 0) {
+      needToRecalculate = true
+    }
+  }
+  if (Object.prototype.hasOwnProperty.call(date, 'weeks')) {
+    const weeks = _.isNil(date.weeks) ? 0 : _.isNumber(date.weeks) ? date.weeks : parseFloat(date.weeks!)
+    const fullYearsInWeeks = Math.floor(weeks / 52)
+    const leftOversInWeeks = weeks - (fullYearsInWeeks * 52)
+
+    res.days += (fullYearsInWeeks === 0 ? weeks * 7.0 : (fullYearsInWeeks * 360) + (leftOversInWeeks * 7))
+
+    if (weeks !== 0) {
+      needToRecalculate = true
+    }
+  }
+
+  if (!needToRecalculate && (res.days > 30 || res.months > 11 || isFloat(res.days) || isFloat(res.months) || isFloat(res.years))) {
+    needToRecalculate = true
+  }
+  if (needToRecalculate) {
+    const allInDays = res.days + res.months * 30 + res.years * 360
+    const leftoverInMonths = Math.floor(allInDays / 30)
+    res.days = Math.round(allInDays % 30)
+    res.years = Math.floor(leftoverInMonths / 12)
+    res.months = Math.round(leftoverInMonths % 12)
+  }
+
+  if (outputAsString) {
+    return {
+      days: res.days === 0 ? '' : String(res.days),
+      months: res.months === 0 ? '' : String(res.months),
+      years: res.years === 0 ? '' : String(res.years)
+    }
+  }
+  return res
 }
 
 const dateDecimal = (date: DateDiff, outputAsString = false): FormattedDateDiff => {
