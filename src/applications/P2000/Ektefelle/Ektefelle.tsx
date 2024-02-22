169c8162634604540c3274cc2a71ce20d7f140e0
Rittik Dey <dey.rittik@nav.no>
2024-02-22 12:25:24 +0100

    b - Added any type to ektefelle

--- src/applications/P2000/Ektefelle/Ektefelle.tsx
+++ src/applications/P2000/Ektefelle/Ektefelle.tsx
@@ -34,7 +34,7 @@ const Ektefelle: React.FC<MainFormProps> = ({
   const { validation } = useAppSelector(mapState)
   const namespace = `${parentNamespace}-ektefelle`
   const target = 'nav.ektefelle'
-  const ektefelle:  P2000Ektefelle | undefined = _.get(PSED, target)
+  const ektefelle:  P2000Ektefelle | undefined | any = _.get(PSED, target)
 
   useUnmount(() => {
     const clonedvalidation = _.cloneDeep(validation)
