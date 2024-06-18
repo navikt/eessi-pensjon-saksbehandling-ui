2257a846b739a9e31cc6f49cfc0aaa92388f562e
K153294 <martin.knutsen@nav.no>
2024-06-18 14:13:26 +0200

    B - Removed unnecessary script files, removed greyed out code

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-node migrateEnvVars.mjs
-
-exec node ./server.mjs
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import legacy from '@vitejs/plugin-legacy'
-/*import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({ path: path.resolve('/tmp/.env') });*/
 
 export default defineConfig({
   base: "/",
@@ -53,6 +49,5 @@ export default defineConfig({
     alias: {
       src: "/src",
     },
-
   },
 })
