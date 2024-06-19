50dac468bb8ae52ad5c4ed17a00ea2581545c221
K153294 <martin.knutsen@nav.no>
2024-06-19 16:24:46 +0200

    B - Logged git commit hash to see if it is being set

--- src/metrics/sentry.ts
+++ src/metrics/sentry.ts
@@ -1,6 +1,8 @@
 import * as Sentry from '@sentry/browser'
 
 export const init = () => {
+
+  console.log("GIT_COMMIT_HASH: " + import.meta.env.GIT_COMMIT_HASH)
   Sentry.init({
     dsn: 'https://d146efc542844eec915ff554fb021498@sentry.gc.nav.no/28',
     release: import.meta.env.GIT_COMMIT_HASH || 'unknown',
