87473f766508386361806fc40c48d0cc59f89272
K153294 <martin.knutsen@nav.no>
2024-06-19 14:57:07 +0200

    B - Tried to use MODE instead of NODE_ENV

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,4 +1,4 @@
 export default import.meta.env.NODE_ENV
-export const IS_DEVELOPMENT = import.meta.env.NODE_ENV === 'development'
-export const IS_PRODUCTION = import.meta.env.NODE_ENV === 'production'
-export const IS_TEST = import.meta.env.NODE_ENV === 'test'
+export const IS_DEVELOPMENT = import.meta.env.MODE === 'development'
+export const IS_PRODUCTION = import.meta.env.MODE === 'production'
+export const IS_TEST = import.meta.env.MODE === 'test'
