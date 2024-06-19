d80f9397403c59f984831ce4ddbc7a8822a34300
K153294 <martin.knutsen@nav.no>
2024-06-19 15:05:38 +0200

    B - Removed logging uses when debugging

--- src/index.tsx
+++ src/index.tsx
@@ -23,20 +23,12 @@ import i18n from './i18n'
 import store from './store'
 import {GJENNY, PESYS} from "./constants/constants";
 
-console.log("The content of NODE_ENV: " + import.meta.env.NODE_ENV)
-console.log("IS_PRODUCTION was set to: " + IS_PRODUCTION)
-
-console.log("VITE_EESSI_PENSJON_FRONTEND_API_URL loaded with process.env: " + process.env.VITE_EESSI_PENSJON_FRONTEND_API_URL)
-console.log("VITE_EESSI_PENSJON_FRONTEND_API_URL loaded with import.meta.env: " + import.meta.env.VITE_EESSI_PENSJON_FRONTEND_API_URL)
-
 if (!IS_PRODUCTION) {
 
   // const axe = require('@axe-core/react')
   // axe(React, ReactDOM, 1000, {})
-  console.log("Inside if (!IS_PRODUCTION) " + IS_PRODUCTION)
 
 } else {
-  console.log("Inside else (!IS_PRODUCTION) " + IS_PRODUCTION)
   Sentry.init()
   Amplitude.init()
 }
