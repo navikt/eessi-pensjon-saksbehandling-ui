39db7231a1aabe9d56209ded9a0c58cf01bf86d3
K153294 <martin.knutsen@nav.no>
2024-07-09 10:41:57 +0200

    R - Removed console log

--- src/metrics/sentry.ts
+++ src/metrics/sentry.ts
@@ -1,8 +1,6 @@
 import * as Sentry from '@sentry/browser'
 
 export const init = () => {
-
-  console.log("GIT_COMMIT_HASH: " + import.meta.env.GIT_COMMIT_HASH)
   Sentry.init({
     dsn: 'https://d146efc542844eec915ff554fb021498@sentry.gc.nav.no/28',
     release: import.meta.env.GIT_COMMIT_HASH || 'unknown',
