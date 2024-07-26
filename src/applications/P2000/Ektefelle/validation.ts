f27c7ca0b7292d134776d88736193d9f8395ab83
Rittik Dey <dey.rittik@nav.no>
2024-07-26 16:11:46 +0200

    b - P2000: Add validation for Ektefelle

--- src/applications/P2000/Ektefelle/validation.ts
+++ src/applications/P2000/Ektefelle/validation.ts
@@ -1,5 +1,6 @@
 import {Validation} from "declarations/app";
 import {Ektefelle} from "../../../declarations/p2000";
+import {checkIfNotEmpty} from "../../../utils/validation";
 
 
 
@@ -16,8 +17,31 @@ export const validateEktefelle = (
 ): boolean => {
   const hasErrors: Array<boolean> = []
 
-  console.log(v, namespace, ektefelle
-  )
+  if(ektefelle?.type){
+    hasErrors.push(checkIfNotEmpty(v, {
+      needle: ektefelle?.person.etternavn,
+      id: namespace + '-person-etternavn',
+      message: 'validation:missing-p2000-person-etternavn'
+    }))
+
+    hasErrors.push(checkIfNotEmpty(v, {
+      needle: ektefelle?.person.fornavn,
+      id: namespace + '-person-fornavn',
+      message: 'validation:missing-p2000-person-fornavn'
+    }))
+
+    hasErrors.push(checkIfNotEmpty(v, {
+      needle: ektefelle?.person.foedselsdato,
+      id: namespace + '-person-foedselsdato',
+      message: 'validation:missing-p2000-person-foedselsdato'
+    }))
+
+    hasErrors.push(checkIfNotEmpty(v, {
+      needle: ektefelle?.person.kjoenn,
+      id: namespace + '-person-kjoenn',
+      message: 'validation:missing-p2000-person-kjoenn'
+    }))
+  }
 
   return hasErrors.find(value => value) !== undefined
 }
