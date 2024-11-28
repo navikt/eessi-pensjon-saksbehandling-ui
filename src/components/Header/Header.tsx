e44907a31503791818e39c41836ad9bc0e24663d
Rittik Dey <dey.rittik@nav.no>
2024-11-28 15:46:50 +0100

    r - Remove Hoykontrast from Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,12 @@
-import { HorizontalSeparatorDiv } from '@navikt/hoykontrast'
 import * as routes from 'src/constants/routes'
 import PT from 'prop-types'
 import { ExclamationmarkTriangleIcon, PersonSuitFillIcon } from '@navikt/aksel-icons'
-import _ from 'lodash'
 import { useTranslation } from 'react-i18next'
 import NavLogoTransparent from 'src/assets/images/NavLogoTransparent'
-import { Loader, Heading } from '@navikt/ds-react'
+import {Loader, HStack, Spacer} from '@navikt/ds-react'
 import styled from 'styled-components'
 import {GJENNY} from "src/constants/constants";
+import {CenterHStack} from "src/components/StyledComponents";
 
 export interface HeaderProps {
   className ?: string
@@ -19,19 +18,10 @@ export interface HeaderProps {
   indexType?: string
 }
 
-const BrandDiv = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-`
-const HeaderDiv = styled.header`
+const HeaderDiv = styled(HStack)`
   background-color: var(--a-gray-900);
-  display: flex;
-  flex-direction: row;
   height: 4rem;
-  justify-content: space-between;
   align-items: center;
-  flex-shrink: 0;
 `
 const NameDiv = styled.div`
   margin: auto 0px;
@@ -54,12 +44,6 @@ const Title = styled.div`
   padding-left: 15px;
 `
 
-const UserDiv = styled.div`
-  align-items: flex-end;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
 const UsernameSpan = styled.span`
   padding: 0.45rem;
   padding-left: 0.5rem;
@@ -68,7 +52,7 @@ const UsernameSpan = styled.span`
 `
 
 const Header: React.FC<HeaderProps> = ({
-  className, children, gettingUserInfo, header, isLoggingOut, username, indexType = "PESYS"
+  className, gettingUserInfo, isLoggingOut, username, indexType = "PESYS"
 }: HeaderProps): JSX.Element => {
   const { t } = useTranslation()
 
@@ -78,62 +62,52 @@ const Header: React.FC<HeaderProps> = ({
 
   return (
     <HeaderDiv role='banner' className={className}>
-      <BrandDiv>
-        <a
-          href='#index'
-          data-testid='c-header--logo-link'
-          onClick={onLogoClick}
-        >
-          <NavLogoTransparent width='100' height='45' color='white' />
-        </a>
-        <Skillelinje />
-        <Title>
-          <span>{indexType === GJENNY ? t('ui:app-headerTitle-gjenny') : t('ui:app-headerTitle')}</span>
-        </Title>
-      </BrandDiv>
-      <UserDiv>
-        {isLoggingOut
-          ? <Loader type='xsmall' />
-          : (
-            <>
-              <SaksbehandlerUser>
-                <PersonSuitFillIcon fontSize="1.5rem" />
-              </SaksbehandlerUser>
-              <HorizontalSeparatorDiv />
-            </>
-            )}
-        <Skillelinje />
-        <NameDiv>
-          {gettingUserInfo
-            ? t('message:loading-gettingUserInfo')
+        <CenterHStack>
+          <a
+            href='#index'
+            data-testid='c-header--logo-link'
+            onClick={onLogoClick}
+          >
+            <NavLogoTransparent width='100' height='45' color='white' />
+          </a>
+          <Skillelinje />
+          <Title>
+            <span>{indexType === GJENNY ? t('ui:app-headerTitle-gjenny') : t('ui:app-headerTitle')}</span>
+          </Title>
+        </CenterHStack>
+        <Spacer/>
+        <CenterHStack>
+          {isLoggingOut
+            ? <Loader type='xsmall' />
             : (
-                username
-                  ? (
-                    <UsernameSpan>
-                      {username}
-                    </UsernameSpan>
-                    )
-                  : (
-                    <>
-                      <ExclamationmarkTriangleIcon fontSize="1.5rem" />
-                      <UsernameSpan>
-                        {t('ui:unknown')}
-                      </UsernameSpan>
-                    </>
-                    )
+              <>
+                <SaksbehandlerUser>
+                  <PersonSuitFillIcon fontSize="1.5rem" />
+                </SaksbehandlerUser>
+              </>
               )}
-        </NameDiv>
-      </UserDiv>
-      {header && (
-        _.isString(header)
-          ? (
-            <Heading size='medium'>
-              {header}
-            </Heading>
-            )
-          : header
-      )}
-      {children}
+          <Skillelinje />
+          <NameDiv>
+            {gettingUserInfo
+              ? t('message:loading-gettingUserInfo')
+              : (
+                  username
+                    ? (
+                      <UsernameSpan>
+                        {username}
+                      </UsernameSpan>
+                      )
+                    : (
+                      <>
+                        <ExclamationmarkTriangleIcon fontSize="1.5rem" />
+                        <UsernameSpan>
+                          {t('ui:unknown')}
+                        </UsernameSpan>
+                      </>
+                      )
+                )}
+          </NameDiv>
+        </CenterHStack>
     </HeaderDiv>
   )
 }
