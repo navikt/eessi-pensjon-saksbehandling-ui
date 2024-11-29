10252a34532c86c71f84208a0f754193c3b44cfc
martinvkn <martin.knutsen@nav.no>
2024-11-29 14:03:38 +0100

    Removed hoykontrast elements from P5000HelpModal

--- src/applications/P5000/components/P5000HelpModal.tsx
+++ src/applications/P5000/components/P5000HelpModal.tsx
@@ -1,7 +1,6 @@
 import Modal from 'src/components/Modal/Modal'
 import { ChevronRightIcon , TrashIcon } from '@navikt/aksel-icons'
-import { BodyLong, Button } from '@navikt/ds-react'
-import { VerticalSeparatorDiv } from '@navikt/hoykontrast'
+import {BodyLong, Box, Button, VStack} from '@navikt/ds-react'
 import { useTranslation } from 'react-i18next'
 import P5000HelpPNG from 'src/assets/images/p5000help.png'
 
@@ -16,56 +15,55 @@ const P5000HelpModal = ({
         modalTitle: t('p5000:saved-working-copy') + ' ' + t('ui:hva-betyr-det'),
         modalContent: (
           <div style={{ maxWidth: '800px' }}>
-            <VerticalSeparatorDiv size='2' />
-            <BodyLong>
-              {t('p5000:help-modal-1')}
-              <Button
-                size='small'
-                variant='tertiary'
-                onClick={() => {}}
-                iconPosition="right" icon={<ChevronRightIcon aria-hidden />}
-              >
-                {t('p5000:registrert')}
-              </Button>
-              {t('p5000:help-modal-2')}
-            </BodyLong>
-            <VerticalSeparatorDiv />
-            <BodyLong>
-              {t('p5000:help-modal-3')}
-            </BodyLong>
-            <VerticalSeparatorDiv />
-            <BodyLong>
-              {t('p5000:help-modal-4')}
-            </BodyLong>
-            <VerticalSeparatorDiv />
-            <div style={{ textAlign: 'center' }}>
-              <img alt='p5000help' src={P5000HelpPNG} />
-            </div>
-            <VerticalSeparatorDiv />
-            <BodyLong>
-              {t('p5000:help-modal-5')}
-              <Button
-                size='small'
-                variant='tertiary'
-                onClick={() => {}}
-                iconPosition="right" icon={<ChevronRightIcon aria-hidden />}
-              >
-                {t('p5000:rediger')}
-              </Button>
-              {t('p5000:help-modal-6')}
-              <Button
-                size='small'
-                variant='tertiary'
-                onClick={() => {}}
-                iconPosition="left" icon={<TrashIcon aria-hidden />}
-              >
-                {t('ui:remove')}
-              </Button>
-              .
-            </BodyLong>
-            <BodyLong>
-              {t('p5000:help-modal-7')}
-            </BodyLong>
+            <Box paddingBlock="8 0">
+              <VStack gap="4">
+                <BodyLong>
+                  {t('p5000:help-modal-1')}
+                  <Button
+                    size='small'
+                    variant='tertiary'
+                    onClick={() => {}}
+                    iconPosition="right" icon={<ChevronRightIcon aria-hidden />}
+                  >
+                    {t('p5000:registrert')}
+                  </Button>
+                  {t('p5000:help-modal-2')}
+                </BodyLong>
+                <BodyLong>
+                  {t('p5000:help-modal-3')}
+                </BodyLong>
+                <BodyLong>
+                  {t('p5000:help-modal-4')}
+                </BodyLong>
+                <div style={{ textAlign: 'center' }}>
+                  <img alt='p5000help' src={P5000HelpPNG} />
+                </div>
+                <BodyLong>
+                  {t('p5000:help-modal-5')}
+                  <Button
+                    size='small'
+                    variant='tertiary'
+                    onClick={() => {}}
+                    iconPosition="right" icon={<ChevronRightIcon aria-hidden />}
+                  >
+                    {t('p5000:rediger')}
+                  </Button>
+                  {t('p5000:help-modal-6')}
+                  <Button
+                    size='small'
+                    variant='tertiary'
+                    onClick={() => {}}
+                    iconPosition="left" icon={<TrashIcon aria-hidden />}
+                  >
+                    {t('ui:remove')}
+                  </Button>
+                  .
+                </BodyLong>
+                <BodyLong>
+                  {t('p5000:help-modal-7')}
+                </BodyLong>
+              </VStack>
+            </Box>
           </div>
         ),
         modalButtons: [{
