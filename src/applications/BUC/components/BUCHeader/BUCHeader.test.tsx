f70997574dd8f4efab69f1eb056c47607ad13091
martinvkn <martin.knutsen@nav.no>
2025-03-28 15:51:51 +0100

    B - Fixed test in BUCHeader test

--- src/applications/BUC/components/BUCHeader/BUCHeader.test.tsx
+++ src/applications/BUC/components/BUCHeader/BUCHeader.test.tsx
@@ -34,31 +34,14 @@ describe('applications/BUC/components/BUCHeader/BUCHeader', () => {
 
   it('Render: has proper HTML structure', () => {
     render(<BUCHeader {...initialMockProps} />)
-    expect(screen.getByTestId('a_buc_c_BUCHeader--' + buc.type + '_' + buc.caseId)).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--title_id')).toHaveTextContent('P_BUC_01 - buc:buc-P_BUC_01')
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_date_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_date_id')).toHaveTextContent(
+    expect(screen.getByText(
       'ui:created: ' + moment(new Date(buc.startDate as number)).format('DD.MM.YYYY')
-    )
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_owner_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_owner_institution_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_owner_id')).toHaveTextContent(
-      'buc:form-caseOwner: NO:NAVAT07'
-    )
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_case_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_case_id')).toHaveTextContent('buc:form-caseNumberInRina: 600891')
+    ))
+    expect(screen.getByRole('heading', )).toBeInTheDocument()
+    expect(screen.queryByRole('button')).not.toBeInTheDocument()
 
-    expect(screen.getByTestId('a_buc_c_BUCHeader--label_avdod_id')).not.toBeInTheDocument()
+    expect(screen.getByText('' + buc.seds?.filter(sedFilter).length))
 
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_id FlagList')).toBeInTheDocument() // TODO
-
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_numberofseds_id')).toBeInTheDocument()
-
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_numberofseds_id')).toHaveTextContent('' + buc.seds?.filter(sedFilter).length)
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_tags_id')).toBeInTheDocument()
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_tags_id ProblemCircleIcon')).toBeInTheDocument()
   })
 
   it('Render: shows icon if we have tags', () => {
