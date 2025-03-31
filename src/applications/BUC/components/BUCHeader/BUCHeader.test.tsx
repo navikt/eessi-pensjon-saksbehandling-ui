39538bc205f9a452cd8ee393ecc78efc162e4c5a
martinvkn <martin.knutsen@nav.no>
2025-03-31 15:28:28 +0200

    T - Fixed, refactored or deleted rest of the tests in BUCHeader

--- src/applications/BUC/components/BUCHeader/BUCHeader.test.tsx
+++ src/applications/BUC/components/BUCHeader/BUCHeader.test.tsx
@@ -1,4 +1,3 @@
-import { sedFilter } from 'src/applications/BUC/components/BUCUtils/BUCUtils'
 import { Buc, BucsInfo } from 'src/declarations/buc'
 import { render, screen, fireEvent } from '@testing-library/react'
 import mockBucs from 'src/mocks/buc/bucs'
@@ -27,42 +26,23 @@ describe('applications/BUC/components/BUCHeader/BUCHeader', () => {
     stageSelector(defaultSelector, {})
   })
 
-  it('Render: match snapshot', () => {
-    const { container } = render(<BUCHeader {...initialMockProps} />)
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
   it('Render: has proper HTML structure', () => {
     render(<BUCHeader {...initialMockProps} />)
-    expect(screen.getByText(
-      'ui:created: ' + moment(new Date(buc.startDate as number)).format('DD.MM.YYYY')
-    ))
     expect(screen.getByRole('heading', )).toBeInTheDocument()
     expect(screen.queryByRole('button')).not.toBeInTheDocument()
-
-    expect(screen.getByText('' + buc.seds?.filter(sedFilter).length))
-
-  })
-
-  it('Render: shows icon if we have tags', () => {
-    render(<BUCHeader {...initialMockProps} />)
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_tags_id')).toBeInTheDocument()
-    const mockProps = {
-      ...initialMockProps,
-      bucInfo: {
-        tags: []
-      },
-      buc: {
-        ...buc,
-        seds: []
-      } as Buc
-    }
-    // @ts-ignore
-    render(<BUCHeader {...mockProps} />)
-    expect(screen.getByTestId('a_buc_c_BUCHeader--icon_tags_id')).not.toBeInTheDocument()
+    expect(screen.getByText(
+      (content) => content.startsWith(buc.type!))
+    )
+    expect(screen.getByText(
+      'ui:created: ' + moment(new Date(buc.startDate as number)).format('DD.MM.YYYY'))
+    )
+    expect(screen.getByText('buc:form-caseOwner:'))
+    expect(screen.queryByText(buc.creator?.institution!))
+    expect(screen.getByText(buc.caseId!))
   })
 
   it('Handling: open Rina link', () => {
+    render(<BUCHeader {...initialMockProps} />)
     window.open = jest.fn()
     fireEvent.click(screen.getByTestId('a_buc_c_BUCHeader--label_case_gotorina_link_id'))
     expect(window.open).toHaveBeenCalledWith(defaultSelector.rinaUrl! + initialMockProps.buc.caseId, 'rinaWindow')
