8439f27f9dcca82ffed02c76a1ca13e14c20d674
martinvkn <martin.knutsen@nav.no>
2025-04-04 13:59:08 +0200

    F- Implemented NavLogoTransparent from Aksel for page header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,8 @@
-import * as routes from 'src/constants/routes'
 import PT from 'prop-types'
 import { ExclamationmarkTriangleIcon, PersonSuitFillIcon } from '@navikt/aksel-icons'
 import { useTranslation } from 'react-i18next'
 import NavLogoTransparent from 'src/assets/images/NavLogoTransparent'
-import {Loader, HStack, Spacer} from '@navikt/ds-react'
+import {Loader, HStack, Spacer, InternalHeader} from '@navikt/ds-react'
 import styled from 'styled-components'
 import {GJENNY} from "src/constants/constants";
 
@@ -21,37 +20,16 @@ export interface HeaderProps {
   indexType?: string
 }
 
-const HeaderDiv = styled(HStack)`
-  background-color: var(--a-gray-900);
-  height: 4rem;
-  align-items: center;
-`
-const NameDiv = styled.div`
-  margin: auto 0px;
-  padding: 0.3em;
-`
 const SaksbehandlerUser = styled.div`
    color: white;
 `
-const Skillelinje = styled.div`
-  border-left: 1px solid var(--a-white);
-  display: flex;
-  height: 30px;
-  width: 1px;
-  margin-left: 1rem;
-`
+
 const Title = styled.div`
+  align-items: center;
   color: var(--a-white);
   display: flex;
   font-size: 13pt;
   padding-left: 15px;
-`
-
-const UsernameSpan = styled.span`
-  padding: 0.45rem;
-  padding-left: 0.5rem;
-  padding-right: 1.6rem;
-  color: var(--a-white) !important;
 `
 
 const Header: React.FC<HeaderProps> = ({
@@ -59,59 +37,43 @@ const Header: React.FC<HeaderProps> = ({
 }: HeaderProps): JSX.Element => {
   const { t } = useTranslation()
 
-  const onLogoClick = () => {
-    window.location.href = indexType !== GJENNY ? routes.ROOT : routes.GJENNY
-  }
-
   return (
-    <HeaderDiv role='banner' className={className}>
+    <InternalHeader>
+      <InternalHeader.Title
+      >
         <CenterHStack>
-          <a
-            href='#index'
-            data-testid='c-header--logo-link'
-            onClick={onLogoClick}
-          >
-            <NavLogoTransparent width='100' height='45' color='white' />
-          </a>
-          <Skillelinje />
-          <Title>
-            <span>{indexType === GJENNY ? t('ui:app-headerTitle-gjenny') : t('ui:app-headerTitle')}</span>
-          </Title>
+          <NavLogoTransparent width='80' height='36' color='white' />
         </CenterHStack>
-        <Spacer/>
-        <CenterHStack>
-          {isLoggingOut
-            ? <Loader type='xsmall' />
-            : (
-              <>
-                <SaksbehandlerUser>
-                  <PersonSuitFillIcon fontSize="1.5rem" />
-                </SaksbehandlerUser>
-              </>
-              )}
-          <Skillelinje />
-          <NameDiv>
-            {gettingUserInfo
-              ? t('message:loading-gettingUserInfo')
-              : (
-                  username
-                    ? (
-                      <UsernameSpan>
-                        {username}
-                      </UsernameSpan>
-                      )
-                    : (
-                      <>
-                        <ExclamationmarkTriangleIcon fontSize="1.5rem" />
-                        <UsernameSpan>
-                          {t('ui:unknown')}
-                        </UsernameSpan>
-                      </>
-                      )
-                )}
-          </NameDiv>
-        </CenterHStack>
-    </HeaderDiv>
+      </InternalHeader.Title>
+      <Title>
+        <span>{indexType === GJENNY ? t('ui:app-headerTitle-gjenny') : t('ui:app-headerTitle')}</span>
+      </Title>
+      <Spacer />
+      <InternalHeader.User
+        name={
+          gettingUserInfo ?
+            t('message:loading-gettingUserInfo') :
+            username ?
+              username :
+              t('ui:unknown')
+        }
+      />
+      <CenterHStack>
+        {isLoggingOut
+          ? <Loader type='xsmall' />
+          : (
+            <>
+              <SaksbehandlerUser>
+                { username
+                  ? <PersonSuitFillIcon fontSize="1.25rem" />
+                  : <ExclamationmarkTriangleIcon fontSize="1.25rem" />
+                }
+              </SaksbehandlerUser>
+            </>
+          )
+        }
+      </CenterHStack>
+    </InternalHeader>
   )
 }
 
