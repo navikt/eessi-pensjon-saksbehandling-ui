40bd28b01bf1d00574ae5f3e9a02a1067f98643e
martinvkn <martin.knutsen@nav.no>
2025-04-04 16:18:47 +0200

    f - Added box around country and periods fields in CheckboxWithCountryAndPeriods

--- src/applications/P8000/components/CheckboxWithCountryAndPeriods.tsx
+++ src/applications/P8000/components/CheckboxWithCountryAndPeriods.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {HGrid, HStack, TextField, VStack} from "@navikt/ds-react";
+import {Box, HGrid, HStack, TextField, VStack} from "@navikt/ds-react";
 import CountryDropdown from "src/components/CountryDropdown/CountryDropdown";
 import {Country} from "@navikt/land-verktoy";
 import styled from "styled-components";
@@ -11,6 +11,21 @@ import {P8000Field} from "src/declarations/p8000";
 
 export const CountryDiv = styled.div`
   min-width: 200px;
+`
+
+export const CountryAndPeriodsBox = styled(Box)`
+  &.new {
+    background-color: rgba(236, 243, 153, 0.5);
+  };
+  &.error {
+    background-color: rgba(255, 0, 0, 0.2);
+  };
+  &:hover:not(.new):not(.error) {
+    background-color: var(--a-gray-100);
+  }
+  &:not(:hover) .control-buttons {
+    display:none
+  }
 `
 
 export const CheckboxWithCountryAndPeriods: React.FC<P8000FieldComponentProps> = ({
@@ -36,42 +51,45 @@ export const CheckboxWithCountryAndPeriods: React.FC<P8000FieldComponentProps> =
         target={target}
       />
       {checked &&
-        <HGrid
-          columns={4}
-          gap="4"
-          paddingBlock="0 4"
+        <CountryAndPeriodsBox
+          paddingBlock="2 4"
           paddingInline="4 0"
         >
-          <VStack gap="4">
-            {options?.showCountry &&
-              <CountryDiv>
-                <CountryDropdown
-                  error={undefined}
-                  id="land"
-                  countryCodeListName="euEftaLand"
-                  label="Land"
-                  hideLabel={false}
-                  onOptionSelected={(c: Country) => {setProperty('landkode', c.value)}}
-                  values={field?.landkode  ?? ''}
-                />
-              </CountryDiv>
-            }
-            {options?.showPeriod &&
-              <HStack
-                gap="4"
-                wrap={false}
-              >
-                <TextField label="Fra" hideLabel={false} value={field?.periodeFra} onChange={(e) => setProperty('periodeFra', e.target.value)}/>
-                <TextField label="Til" hideLabel={false} value={field?.periodeTil} onChange={(e) => setProperty('periodeTil', e.target.value)}/>
-              </HStack>
-            }
-            {options?.showMonths &&
-              <>
-                <TextField label="Antall måneder" hideLabel={false} value={field?.antallMaaneder} onChange={(e) => setProperty('antallMaaneder', e.target.value)}/>
-              </>
-            }
-           </VStack>
-        </HGrid>
+          <HGrid
+            columns={4}
+            gap="4"
+          >
+            <VStack gap="4">
+              {options?.showCountry &&
+                <CountryDiv>
+                  <CountryDropdown
+                    error={undefined}
+                    id="land"
+                    countryCodeListName="euEftaLand"
+                    label="Land"
+                    hideLabel={false}
+                    onOptionSelected={(c: Country) => {setProperty('landkode', c.value)}}
+                    values={field?.landkode  ?? ''}
+                  />
+                </CountryDiv>
+              }
+              {options?.showPeriod &&
+                <HStack
+                  gap="4"
+                  wrap={false}
+                >
+                  <TextField label="Fra" hideLabel={false} value={field?.periodeFra} onChange={(e) => setProperty('periodeFra', e.target.value)}/>
+                  <TextField label="Til" hideLabel={false} value={field?.periodeTil} onChange={(e) => setProperty('periodeTil', e.target.value)}/>
+                </HStack>
+              }
+              {options?.showMonths &&
+                <>
+                  <TextField label="Antall måneder" hideLabel={false} value={field?.antallMaaneder} onChange={(e) => setProperty('antallMaaneder', e.target.value)}/>
+                </>
+              }
+             </VStack>
+          </HGrid>
+        </CountryAndPeriodsBox>
       }
     </VStack>
   )
