4b2bd7169674ad37327a432847c1c6812f8a4f0a
martinvkn <martin.knutsen@nav.no>
2025-04-04 16:38:06 +0200

    f - Removed links from ContextBanner

--- src/components/ContextBanner/ContextBanner.tsx
+++ src/components/ContextBanner/ContextBanner.tsx
@@ -1,23 +1,16 @@
 import WaitingPanel from 'src/components/WaitingPanel/WaitingPanel'
-import { BUCMode, PesysContext } from 'src/declarations/app.d'
+import { PesysContext } from 'src/declarations/app.d'
 import { SakTypeMap, SakTypeValue } from 'src/declarations/buc.d'
 import { PersonPDL } from 'src/declarations/person.d'
 import { State } from 'src/declarations/reducers'
-import { linkLogger, standardLogger } from 'src/metrics/loggers'
-import { ChevronRightIcon, ExternalLinkIcon } from '@navikt/aksel-icons'
-import {HStack, Link} from '@navikt/ds-react'
+import { standardLogger } from 'src/metrics/loggers'
+import { ChevronRightIcon } from '@navikt/aksel-icons'
+import { HStack } from '@navikt/ds-react'
 import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
-export const DivWithLinks = styled(HStack)`
-  padding: 0.5rem 2rem;
-  flex-direction: row-reverse;
-`
-const SeparatorSpan = styled.span`
-  padding: 0rem 0.5rem
-`
 const Content = styled(HStack)`
   align-items: center;
   justify-content: space-between;
@@ -33,12 +26,7 @@ export const Tag = styled(HStack)`
  font-size: var(--a-font-size-small)
 `
 
-export const LinkContent = styled(HStack)`
- align-items: center;
-`
-
 export interface ContextBannerProps {
-  mode: BUCMode
 }
 
 export interface ContextBannerSelector {
@@ -56,7 +44,6 @@ const mapState = (state: State): ContextBannerSelector => ({
 })
 
 const ContextBanner: React.FC<ContextBannerProps> = ({
-  mode
 }: ContextBannerProps): JSX.Element => {
   const { t } = useTranslation()
   const { gettingSakType, pesysContext, sakType }: ContextBannerSelector = useSelector<State, ContextBannerSelector>(mapState)
@@ -90,33 +77,6 @@ const ContextBanner: React.FC<ContextBannerProps> = ({
           )}
         </Tag>
       </Context>
-      <DivWithLinks data-testid="div-with-links">
-        <Link
-          target='_blank'
-          data-amplitude='links.rettskilder'
-          href='https://lovdata.no/pro/#document/NAV/rundskriv/v2-45-03'
-          onClick={(e: React.MouseEvent) => linkLogger(e, { mode })}
-        >
-          <LinkContent gap="4">
-            {t('ui:lawsource')}
-            <ExternalLinkIcon fontSize="1.5rem" />
-          </LinkContent>
-        </Link>
-        <SeparatorSpan>
-          •
-        </SeparatorSpan>
-        <Link
-          target='_blank'
-          data-amplitude='links.hjelpe'
-          href='https://navno.sharepoint.com/sites/fag-og-ytelser-regelverk-og-rutiner/SitePages/Pensjon-.aspx'
-          onClick={(e: React.MouseEvent) => linkLogger(e, { mode })}
-        >
-          <LinkContent gap="4">
-            {t('ui:help')}
-            <ExternalLinkIcon fontSize="1.5rem" />
-          </LinkContent>
-        </Link>
-      </DivWithLinks>
     </Content>
   )
 }
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,11 +1,8 @@
 import PersonPanel from 'src/applications/PersonPanel/PersonPanel'
 import ContextBanner from 'src/components/ContextBanner/ContextBanner'
 import TopContainer from 'src/components/TopContainer/TopContainer'
-import { BUCMode } from 'src/declarations/app'
-import { State } from 'src/declarations/reducers'
 import { timeLogger } from 'src/metrics/loggers'
 import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
 import {Box, VStack} from "@navikt/ds-react";
 import BUCIndex from 'src/applications/BUC'
 import {GJENNY, PESYS} from "../../constants/constants";
@@ -17,18 +14,7 @@ export interface IndexPageProps {
   indexType?: string
 }
 
-export interface IndexPageSelector {
-  mode: BUCMode
-  username: string | undefined
-}
-
-const mapState = (state: State): IndexPageSelector => ({
-  mode: state.buc.mode,
-  username: state.app.username
-})
-
 export const IndexPage: React.FC<IndexPageProps> = ({indexType = "PESYS"}): JSX.Element => {
-  const { mode }: IndexPageSelector = useSelector<State, IndexPageSelector>(mapState)
   const [loggedTime] = useState<Date>(new Date())
 
   useEffect(() => {
@@ -40,7 +26,7 @@ export const IndexPage: React.FC<IndexPageProps> = ({indexType = "PESYS"}): JSX.
   return (
     <TopContainer indexType={indexType}>
       <VStack gap="4">
-        <ContextBanner mode={mode} />
+        <ContextBanner />
           <Box padding="4">
             <VStack>
               <PersonPanel />
