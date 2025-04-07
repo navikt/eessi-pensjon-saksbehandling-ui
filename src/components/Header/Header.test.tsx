57d54c04e68f7aa4d9b7468c3971ab08bc292a22
martinvkn <martin.knutsen@nav.no>
2025-04-07 14:48:27 +0200

    F - Removed NAV logo from Header

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -2,8 +2,6 @@ import { render, screen } from '@testing-library/react'
 import Header, { HeaderProps } from './Header'
 
 describe('src/components/Header/Header', () => {
-  const NAV_LOGO_TITLE_TEXT = "NAV logo"
-
   const initialMockProps: HeaderProps = {
     username: 'testUser'
   }
@@ -13,9 +11,8 @@ describe('src/components/Header/Header', () => {
   })
 
   it('Handling: has proper HTML structure', () => {
-    expect(screen.queryByRole('img')).toBeInTheDocument()
-    expect(screen.getByText(NAV_LOGO_TITLE_TEXT)).toBeInTheDocument()
     expect(screen.getByText('ui:app-headerTitle' || 'app-headerTitle-gjenny', )).toBeInTheDocument()
+    expect(screen.queryByRole('img')).toBeInTheDocument()
     expect(screen.queryByRole('button')).toBeInTheDocument()
     expect(screen.getByText('ui:app-header-menu-label')).toBeInTheDocument()
     expect(screen.getByText(initialMockProps.username!)).toBeInTheDocument()
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,7 @@
 import {MenuGridIcon, ExternalLinkIcon} from '@navikt/aksel-icons'
 import { useTranslation } from 'react-i18next'
-import NavLogoTransparent from 'src/assets/images/NavLogoTransparent'
-import {ActionMenu, HStack, Spacer, InternalHeader} from '@navikt/ds-react'
-import styled from 'styled-components'
+import {ActionMenu, Spacer, InternalHeader} from '@navikt/ds-react'
 import {GJENNY} from "src/constants/constants";
-
-export const CenterHStack = styled(HStack)`
-  align-items: center;
-`
 
 export interface HeaderProps {
   children?: JSX.Element | Array<JSX.Element | null>
@@ -17,14 +11,6 @@ export interface HeaderProps {
   indexType?: string
 }
 
-const Title = styled.div`
-  align-items: center;
-  color: var(--a-white);
-  display: flex;
-  font-size: 13pt;
-  padding-left: 15px;
-`
-
 const Header: React.FC<HeaderProps> = ({
   gettingUserInfo, username, indexType = "PESYS"
 }: HeaderProps): JSX.Element => {
@@ -32,15 +18,9 @@ const Header: React.FC<HeaderProps> = ({
 
   return (
     <InternalHeader>
-      <InternalHeader.Title
-      >
-        <CenterHStack>
-          <NavLogoTransparent width='80' height='36' color='white' />
-        </CenterHStack>
+      <InternalHeader.Title as="h1">
+        {indexType === GJENNY ? t('ui:app-headerTitle-gjenny') : t('ui:app-headerTitle')}
       </InternalHeader.Title>
-      <Title>
-        <span>{indexType === GJENNY ? t('ui:app-headerTitle-gjenny') : t('ui:app-headerTitle')}</span>
-      </Title>
       <Spacer />
       <ActionMenu>
         <ActionMenu.Trigger>
