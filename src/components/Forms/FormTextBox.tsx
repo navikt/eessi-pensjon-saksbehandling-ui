064d623af63d5c04159aa7c7b60e3b64d309d06a
martinvkn <martin.knutsen@nav.no>
2025-04-11 14:36:43 +0200

    F - P8000: GUI improvements for CheckboxWithCountryAndPeriods and UtenlandskePin

--- src/components/Forms/FormTextBox.tsx
+++ src/components/Forms/FormTextBox.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ErrorLabel from 'src/components/Forms/ErrorLabel'
-import {Box} from "@navikt/ds-react";
+import {Box, Label} from "@navikt/ds-react";
 import {ResponsiveProp} from "@navikt/ds-react/cjs/layout/utilities/types";
 
 
 export interface FormTextProps {
   error: string | undefined
   id: string
+  label?: string
   children: any
   padding?: ResponsiveProp<any>
 }
@@ -15,9 +16,13 @@ const FormTextBox: React.FC<FormTextProps> = ({
   children,
   error,
   id,
+  label,
   padding = "1"
 }: FormTextProps) => (
   <Box tabIndex={0} id={id} padding={padding}>
+    {label &&
+      <Label>{label}</Label>
+    }
     {children}
     <ErrorLabel error={error} />
   </Box>
