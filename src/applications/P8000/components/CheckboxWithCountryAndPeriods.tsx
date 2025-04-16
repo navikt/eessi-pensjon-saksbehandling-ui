dc3d584883843476b576f1dc19b018455d05af84
Rittik Dey <dey.rittik@nav.no>
2025-04-16 13:42:06 +0200

    b - P8000: Fix showMonths - not showing when set for field

--- src/applications/P8000/components/CheckboxWithCountryAndPeriods.tsx
+++ src/applications/P8000/components/CheckboxWithCountryAndPeriods.tsx
@@ -60,10 +60,10 @@ export const CheckboxWithCountryAndPeriods: React.FC<P8000FieldComponentProps> =
               >
                 <TextField style={{width: "5rem"}} label="Fra" hideLabel={false} value={field?.periodeFra} onChange={(e) => setProperty('periodeFra', e.target.value)}/>
                 <TextField style={{width: "5rem"}} label="Til" hideLabel={false} value={field?.periodeTil} onChange={(e) => setProperty('periodeTil', e.target.value)}/>
-                {options?.showMonths &&
-                  <TextField label="Antall måneder" hideLabel={false} value={field?.antallMaaneder} onChange={(e) => setProperty('antallMaaneder', e.target.value)}/>
-                }
               </HStack>
+            }
+            {options?.showMonths &&
+              <TextField label="Antall måneder" hideLabel={false} value={field?.antallMaaneder} onChange={(e) => setProperty('antallMaaneder', e.target.value)}/>
             }
             <Spacer/>
           </HGrid>
