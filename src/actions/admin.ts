5796873ff18285a36d3b4f143a35ed94b7b75602
martinvkn <martin.knutsen@nav.no>
2025-05-26 13:47:22 +0200

    F - Adminpage - revert set headers as text/plain for resendDocumentList

--- src/actions/admin.ts
+++ src/actions/admin.ts
@@ -30,9 +30,6 @@ export const resendDocumentList = (
     expectedPayload: { success: true },
     method: 'POST',
     body: documentList,
-    headers: {
-      'content-type': 'text/plain'
-    },
     type: {
       request: types.ADMIN_RESEND_DOCUMENT_LIST_REQUEST,
       success: types.ADMIN_RESEND_DOCUMENT_LIST_SUCCESS,
