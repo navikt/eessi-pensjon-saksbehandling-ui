bcf1bf488d933fb0809ee85c52858da2ea5e13e6
martinvkn <martin.knutsen@nav.no>
2025-05-28 13:02:00 +0200

    t - Adminpage - test for admin reducers

---
+++ src/reducers/admin.test.ts
@@ -0,0 +1,87 @@
+import * as types from 'src/constants/actionTypes'
+import adminReducer, {initialAdminState} from "src/reducers/admin";
+
+describe('reducers/admin', () => {
+  it('ADMIN_RESET_SUCCESS_MSG', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESET_SUCCESS_MSG
+      })
+    ).toEqual({
+      resendingDocumentSuccess: false,
+      resendingDocumentListSuccess: false
+    })
+  })
+
+  it('ADMIN_RESEND_DOCUMENT_REQUEST', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESEND_DOCUMENT_REQUEST
+      })
+    ).toEqual({
+      ...initialAdminState,
+      resendingDocument: true,
+      resendingDocumentSuccess: false,
+    })
+  })
+
+  it('ADMIN_RESEND_DOCUMENT_SUCCESS', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESEND_DOCUMENT_SUCCESS
+      })
+    ).toEqual({
+      ...initialAdminState,
+      resendingDocument: false,
+      resendingDocumentSuccess: true
+    })
+  })
+
+  it('ADMIN_RESEND_DOCUMENT_FAILURE', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESEND_DOCUMENT_FAILURE
+      })
+    ).toEqual({
+      ...initialAdminState,
+      resendingDocument: false,
+      resendingDocumentSuccess: false
+    })
+  })
+
+  it('ADMIN_RESEND_DOCUMENT_LIST_REQUEST', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESEND_DOCUMENT_LIST_REQUEST
+      })
+    ).toEqual({
+      ...initialAdminState,
+      resendingDocumentList: true,
+      resendingDocumentListSuccess: false,
+    })
+  })
+
+  it('ADMIN_RESEND_DOCUMENT_LIST_SUCCESS', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESEND_DOCUMENT_LIST_SUCCESS
+      })
+    ).toEqual({
+      ...initialAdminState,
+      resendingDocumentList: false,
+      resendingDocumentListSuccess: true
+    })
+  })
+
+  it('ADMIN_RESEND_DOCUMENT_LIST_FAILURE', () => {
+    expect(
+      adminReducer(initialAdminState, {
+        type: types.ADMIN_RESEND_DOCUMENT_LIST_FAILURE
+      })
+    ).toEqual({
+      ...initialAdminState,
+      resendingDocumentList: false,
+      resendingDocumentListSuccess: false
+    })
+  })
+})
