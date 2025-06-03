4a8cdca24f1e0cb27c2c445dc92d48e77d1cbeda
martinvkn <martin.knutsen@nav.no>
2025-06-03 16:17:42 +0200

    T - Fixed the advanced BUCTools tests

--- src/applications/BUC/components/BUCTools/BUCTools.test.tsx
+++ src/applications/BUC/components/BUCTools/BUCTools.test.tsx
@@ -1,8 +1,7 @@
-//import { within } from '@testing-library/dom'
 import { getTagList } from 'src/actions/buc'
 import { Buc, BucInfo, BucsInfo, TagRawList } from 'src/declarations/buc.d'
 import { AllowedLocaleString } from 'src/declarations/app.d'
-import { render, screen/*, fireEvent*/ } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import mockFeatureToggles from 'src/mocks/app/featureToggles'
 import mockBucs from 'src/mocks/buc/bucs'
 import mockTagList from 'src/mocks/buc/tagsList'
@@ -11,7 +10,7 @@ import mockP50001 from 'src/mocks/buc/sed_P5000_small1'
 import mockP50002 from 'src/mocks/buc/sed_P5000_small2'
 import { stageSelector } from 'src/setupTests'
 import BUCTools, { BUCToolsProps } from './BUCTools'
-//import allTags from 'src/constants/tagsList'
+import allTags from 'src/constants/tagsList'
 import { P5000sFromRinaMap } from 'src/declarations/p5000'
 
 jest.mock('src/constants/environment.ts', () => {
@@ -40,7 +39,7 @@ const defaultSelector = {
   tagList: mockTagList as TagRawList
 }
 
-const buc: Buc = mockBucs()[0]
+const buc: Buc = mockBucs()[2]
 const bucInfo: BucInfo = (mockBucsInfo as BucsInfo).bucs['' + buc.caseId]
 
 describe('applications/BUC/components/BUCTools/BUCTools', () => {
@@ -82,26 +81,24 @@ describe('applications/BUC/components/BUCTools/BUCTools', () => {
     expect(getTagList).toHaveBeenCalled()
   })
 
-/*  it('Handling: Changing tags', () => {
+  it('Handling: Changing tags', () => {
     (initialMockProps.onTagChange as jest.Mock).mockReset()
     render(<BUCTools {...initialMockProps} initialTab='tags' />)
-    const select = within(screen.getByTestId('a_buc_c_BUCStart--tags-select-id')).getByRole('input')
+    const select = screen.getByRole('combobox')
     fireEvent.keyDown(select, { key: 'ArrowDown' })
     fireEvent.keyDown(select, { key: 'ArrowDown' })
     fireEvent.keyDown(select, { key: 'Enter' })
 
     expect(initialMockProps.onTagChange).toHaveBeenCalledWith([{
-      label: 'buc:tag-refusjonskrav',
-      value: 'tag-refusjonskrav'
-    }, {
-      label: 'buc:' + allTags[1],
-      value: allTags[1]
+      label: 'buc:' + allTags[0],
+      value: allTags[0]
     }])
   })
 
   it('Handling: Loads SEDs for P5000', () => {
-    (initialMockProps.setMode as jest.Mock).mockReset()
+    render(<BUCTools {...initialMockProps} initialTab='P5000' />)
     fireEvent.click(screen.getByTestId('a_buc_c_buctools--P5000-button-id'))
     expect(initialMockProps.setMode).toHaveBeenCalled()
-  })*/
+    screen.debug()
+  })
 })
