ada6b9b5a6f69a7d7eb02dac7e93b8175b8d8750
martinvkn <martin.knutsen@nav.no>
2025-06-24 10:07:52 +0200

    t - Fixed IndexPage tests

--- src/pages/IndexPage/IndexPage.test.tsx
+++ src/pages/IndexPage/IndexPage.test.tsx
@@ -2,14 +2,22 @@ import {  screen, render } from '@testing-library/react'
 
 import { IndexPage, IndexPageProps } from './IndexPage'
 
-jest.mock('components/TopContainer/TopContainer', () => {
+jest.mock('src/constants/environment.ts', () => {
+  return {
+    IS_DEVELOPMENT: 'development',
+    IS_PRODUCTION: 'production',
+    IS_TEST: 'test'
+  };
+})
+
+jest.mock('src/components/TopContainer/TopContainer', () => {
   return ({ children }: {children: JSX.Element}) => (
     <div className='mock-c-topcontainer' data-testid="topcontainer">
       {children}
     </div>
   )
 })
-jest.mock('components/ContextBanner/ContextBanner', () => {
+jest.mock('src/components/ContextBanner/ContextBanner', () => {
   return ({ children }: {children: JSX.Element}) => (
     <div className='mock-c-contextbanner'>
       {children}
@@ -17,20 +25,21 @@ jest.mock('components/ContextBanner/ContextBanner', () => {
   )
 })
 
-describe('pages/IndexPage', () => {
+jest.mock('src/applications/PersonPanel/PersonPanel', () => {
+  return ({ children }: {children: JSX.Element}) => (
+    <div className='mock-c-personpanel' data-testid="personpanel">
+      {children}
+    </div>
+  )
+})
+
+describe('src/pages/IndexPage', () => {
   const initialMockProps: IndexPageProps = {}
 
-  beforeEach(() => {
-    render(<IndexPage {...initialMockProps} />)
-  })
-
-
-  it('Render: match snapshot', () => {
-    const { container } = render(<IndexPage {...initialMockProps} />)
-    expect(container.firstChild).toMatchSnapshot()
-  })
-
   it('Render: has proper HTML structure', () => {
+    render(<IndexPage {...initialMockProps} />)
+
     expect(screen.getByTestId("topcontainer")).toBeTruthy()
+    expect(screen.getByTestId("personpanel")).toBeTruthy()
   })
 })
