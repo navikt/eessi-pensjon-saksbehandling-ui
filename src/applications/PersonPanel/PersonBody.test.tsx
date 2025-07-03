2570fa1bce62687246cebc12c2071323140707a3
martinvkn <martin.knutsen@nav.no>
2025-07-03 14:38:59 +0200

    t - Fixed PersonBody tests

--- src/applications/PersonPanel/PersonBody.test.tsx
+++ src/applications/PersonPanel/PersonBody.test.tsx
@@ -15,7 +15,7 @@ describe('applications/PersonPanel/PersonBody', () => {
     person: {
       ...mockPerson,
       bostedsadresse: null,
-      foedsel: {}
+      foedselsdato: {}
     },
     personAvdods: personAvdod(1)
   }
@@ -31,7 +31,7 @@ describe('applications/PersonPanel/PersonBody', () => {
     expect(screen.getByTitle("ui:marital-status")).toBeInTheDocument()
     expect(screen.getByTestId("w-overview-PersonBody--element-bostedsadresse")).toHaveTextContent(/ui:bostedsadresse:ui:fram-og-til01.01.2020 - 01.01.2021ui:adressenavnAdressenavnui:husnummer00ui:husbokstavAui:poststed0768ui:cityOSLO/i)
     expect(screen.getByTestId("w-overview-PersonBody--element-birthdate")).toHaveTextContent(/ui:birthdate:09.02.1980/i)
-    expect(screen.getByTestId("w-overview-PersonBody--element-nationality")).toHaveTextContent(/ui:nationality:Norge \(09.02.1980\)/i)
+    expect(screen.getByTestId("w-overview-PersonBody--element-nationality")).toHaveTextContent("ui:nationality:Danmark, Norge (09.02.1980 - 09.02.1981), Belgia")
   })
 
   it('Render: Empty value renders not registered', () => {
