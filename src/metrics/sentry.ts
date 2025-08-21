01529db99e0a106b40452713c4110c188bd5ff5a
Rittik Dey <dey.rittik@nav.no>
2025-08-21 13:18:08 +0200

    r - Remove Sentry - not in use

--- src/metrics/sentry.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/browser'
-
-export const init = () => {
-  Sentry.init({
-    dsn: 'https://d146efc542844eec915ff554fb021498@sentry.gc.nav.no/28',
-    release: import.meta.env.GIT_COMMIT_HASH || 'unknown',
-    environment: window.location.hostname
-  })
-}
