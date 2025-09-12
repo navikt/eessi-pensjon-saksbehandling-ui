d3b26378ba169dfddabbdfbd0350d38bbf0b8b94
martinvkn <martin.knutsen@nav.no>
2025-09-12 14:33:49 +0200

    R - Deleted the SpinnerDiv SC

--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -9,13 +9,6 @@ export const SeparatorSpan = styled.span`
   margin-right: 0.25rem;
 `
 
-export const SpinnerDiv = styled.div`
-  position: absolute;
-  top: 3rem;
-  left: 0;
-  right: 0;
-`
-
 export const HorizontalSeparatorSpan = styled.span<{size?: string}>`
   display: inline-block;
   margin-left: ${(props: any) => props.size || 1}rem;
