ef5489604f4232b64a9b9c48aa674e689e241e88
martinvkn <martin.knutsen@nav.no>
2025-11-07 17:45:23 +0100

    F - P5000: Different icons for flagged single and merged periods

--- src/applications/P5000/utils/conversion.tsx
+++ src/applications/P5000/utils/conversion.tsx
@@ -267,6 +267,7 @@ export const mergeP5000ListRows = (
           beregning: groupedBeregning,
           key: 'merge-' + groupedPeriods[key][key2].parent.key,
           flag: !samePeriodSum,
+          flagIkon: "InformationSquareIcon",
           flagLabel: i18n.t('message:warning-periodDoNotMatch'),
           isMergedRow: true
         })
