2f49d9aa19120a39f851919b67330c054a5611a9
martinvkn <martin.knutsen@nav.no>
2025-11-14 20:42:14 +0100

    r - MultipleValueLabel.tsx: Styled Components --> CSS Modules.

--- src/components/MultipleSelect/MultipleValueLabel.tsx
+++ src/components/MultipleSelect/MultipleValueLabel.tsx
@@ -1,16 +1,11 @@
 import { BodyLong } from '@navikt/ds-react'
 import { components } from 'react-select'
-import styled from 'styled-components'
 
-const FlexDiv = styled.div`
-  display: flex;
-  align-items: center;
-`
 const MultipleValueLabel = (props: any) => (
   <components.MultiValueLabel {...props}>
-    <FlexDiv data-testid='c-multipleselect-multivaluelabel'>
+    <div data-testid='c-multipleselect-multivaluelabel'>
       <BodyLong>{props.data.label}</BodyLong>
-    </FlexDiv>
+    </div>
   </components.MultiValueLabel>
 )
 
