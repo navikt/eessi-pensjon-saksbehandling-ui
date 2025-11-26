fea588d5509504d98450487d7727e892eeeb4c47
martinvkn <martin.knutsen@nav.no>
2025-11-26 13:52:59 +0100

    R - LoadingImage: Styled Components --> CSS Modules.

--- src/components/Loading/LoadingImage.tsx
+++ src/components/Loading/LoadingImage.tsx
@@ -1,15 +1,9 @@
-import styled from 'styled-components'
-import { progressBarAnimation } from "src/components/Animations/Animations";
+import styles from './LoadingImage.module.css'
 
-export const ImageDiv = styled.div`
-  min-width: 40px;
-  min-height: 40px;
-  background-color: whitesmoke;
-  border-radius: 100px;
-  transition: width .6s ease;
-  animation: ${progressBarAnimation} 1s linear infinite;
-  background-image: linear-gradient(45deg, hsla(90, 90%, 90%, .15) 25%, transparent 0, transparent 50%, hsla(90, 90%, 90%, .15) 0, hsla(90, 90%, 90%, .15) 75%, transparent 0, transparent);
-`
-const LoadingImage = (props: any) => (<ImageDiv {...props} />)
+const LoadingImage = (props: any) =>
+  (<div
+    className={styles.imageDiv}
+    {...props}
+  />)
 
 export default LoadingImage
