f7d9498bfddb8b3093eb212360734b787bf7fc11
Rittik Dey <dey.rittik@nav.no>
2025-12-04 13:51:11 +0100

    E - Added Knip and removed unused deps ++

--- src/setupProxy.ts
+++
@@ -1,6 +0,0 @@
-const proxy = require('http-proxy-middleware')
-
-module.exports = (app: any) => {
-  app.use(proxy('/frontend/', { target: 'http://localhost:8080', pathRewrite: { '^/frontend': '' }, logLevel: 'silent' }))
-  app.use(proxy('/fagmodul/', { target: 'http://localhost:8081', pathRewrite: { '^/fagmodul': '' }, logLevel: 'silent' }))
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+//@ts-ignore
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 
