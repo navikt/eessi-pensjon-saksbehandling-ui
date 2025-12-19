fccf129b66cfbac5759c3d377b3375f1bf5957e5
Rittik Dey <dey.rittik@nav.no>
2025-12-19 13:50:22 +0100

    E - Fixed TS error

--- src/components/MultipleSelect/MultipleValueLabel.tsx
+++ src/components/MultipleSelect/MultipleValueLabel.tsx
@@ -1,12 +1,15 @@
 import { BodyLong } from '@navikt/ds-react'
 import { components } from 'react-select'
 
-const MultipleValueLabel = (props: any) => (
-  <components.MultiValueLabel {...props}>
-    <div data-testid='c-multipleselect-multivaluelabel'>
-      <BodyLong>{props.data.label}</BodyLong>
-    </div>
-  </components.MultiValueLabel>
-)
+const MultipleValueLabel = (props: any) => {
+  const MultiValueLabel = components.MultiValueLabel as any
+  return (
+    <MultiValueLabel {...props}>
+      <div data-testid='c-multipleselect-multivaluelabel'>
+        <BodyLong>{props.data.label}</BodyLong>
+      </div>
+    </MultiValueLabel>
+  )
+}
 
 export default MultipleValueLabel
