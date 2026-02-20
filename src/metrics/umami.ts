43593f1050827b700e3280f7465d020c6485a2a0
Rittik Dey <dey.rittik@nav.no>
2026-02-20 13:52:19 +0100

    r - Umami: Use commmon navikt taxonomi, add logger for Checkbox

--- src/metrics/umami.ts
+++ src/metrics/umami.ts
@@ -1,3 +1,10 @@
+import {
+  Events,
+  type TaxonomyEvent,
+  AccordionApnetProperties,
+  AvkrysningsboksEndretProperties,
+  KnappKlikketProperties
+} from '@navikt/analytics-types';
 const umamiLogger = (key: string, data: object) => {
   //@ts-ignore
   if (typeof window !== 'undefined' && window.umami) {
@@ -6,10 +13,31 @@ const umamiLogger = (key: string, data: object) => {
   }
 }
 
-export const umamiButtonLogger = (data: object ) => {
-  umamiLogger("knapp klikket", data)
+type TaxonomyEventWithExtra = TaxonomyEvent & {
+  properties?: TaxonomyEvent['properties'] & Record<string, unknown>;
+};
+
+export const logToUmami = (event: TaxonomyEventWithExtra) => {
+  umamiLogger(event.name, event.properties);
 }
 
-export const umamiAccordionLogger = (data: object ) => {
-  umamiLogger("accordion åpnet", data)
+export const umamiButtonLogger = (data: KnappKlikketProperties & Record<string, unknown>) => {
+  logToUmami({
+    name: Events.KNAPP_KLIKKET,
+    properties: data
+  })
+}
+
+export const umamiAccordionLogger = (data: AccordionApnetProperties & Record<string, unknown>) => {
+  logToUmami({
+    name: Events.ACCORDION_APNET,
+    properties: data
+  })
+}
+
+export const umamiCheckBoxLogger = (data: AvkrysningsboksEndretProperties & Record<string, unknown>) => {
+  logToUmami({
+    name: Events.AVKRYSNINGSBOKS_ENDRET,
+    properties: data
+  })
 }
