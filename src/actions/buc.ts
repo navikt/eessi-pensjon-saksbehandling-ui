f93e0348fbef0edc98c7abbe2c74674787e9fe9a
martinvkn <martin.knutsen@nav.no>
2026-04-23 14:25:05 +0200

    F - Added aktoerId to body in fetchBuc call

--- src/actions/buc.ts
+++ src/actions/buc.ts
@@ -278,6 +278,9 @@ export const fetchBuc = (
   return call({
     url,
     expectedPayload: { result: mockBuc(rinaCaseId) },
+    body: {
+      aktoerId
+    },
     context: {
       rinaCaseId,
       aktoerId,
