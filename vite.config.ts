5ac70751eb567a7f7bdb27855d8c3ef4af7f5cf0
martinvkn <martin.knutsen@nav.no>
2026-09-18 15:18:00 +0200

    F - CDN config, first for q (epd)

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",
+  base: process.env.CDN_BASE_URL || '/',
   plugins: [
     react()
   ],
