66f5f59b7b3725a4abdea8efb3b9e66597020736
martinvkn <martin.knutsen@nav.no>
2026-09-24 12:37:59 +0200

    F - Revert set sourcemap variable to false by default (epd)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 //@ts-ignore
 import react from '@vitejs/plugin-react'
 
-const enableSourceMaps = process.env.ENABLE_SOURCEMAPS === 'false'
+const enableSourceMaps = process.env.ENABLE_SOURCEMAPS === 'true'
 
 export default defineConfig({
   base: process.env.CDN_BASE_URL || '/',
